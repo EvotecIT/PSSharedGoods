63102c944689332837e7789ef9c7b128334b2722
PrzemyslawKlys <pklys@evotec.pl>
2018-09-04 19:22:03 +0200

    Update

--- Public/SQL.ps1
+++ Public/SQL.ps1
@@ -4,16 +4,23 @@ function Send-SqlInsert {
         [Object] $Object,
         [hashtable] $SqlSettings
     )
+    $Queries = New-ArrayList
     $ReturnData = @()
     if ($SqlSettings.SqlTableTranspose) {
         $Object = Format-TransposeTable -Object $Object
     }
-    $Queries = New-SqlQuery -Object $Object -SqlSettings $SqlSettings
+    $TableMapping = New-SqlTableMapping -SqlTableMapping $SqlSettings.SqlTableMapping -Object $Object
+
+    if ($SqlSettings.SqlTableCreate) {
+        $CreateTableSQL = New-SqlQueryCreateTable -SqlSettings $SqlSettings -Object $Object -TableMapping $TableMapping
+        Add-ToArray -List $Queries -Element $CreateTableSQL
+    }
+    $Queries += New-SqlQuery -Object $Object -SqlSettings $SqlSettings -TableMapping $TableMapping
     foreach ($Query in $Queries) {
         $ReturnData += $Query
         try {
             if ($Query) {
-                $Data = Invoke-Sqlcmd2 -SqlInstance $SqlSettings.SqlServer -Database $SqlSettings.SqlDatabase -Query $Query -ErrorAction Stop
+                $ReturnData += Invoke-Sqlcmd2 -SqlInstance $SqlSettings.SqlServer -Database $SqlSettings.SqlDatabase -Query $Query -ErrorAction Stop
             }
         } catch {
             $ErrorMessage = $_.Exception.Message -replace "`n", " " -replace "`r", " "
@@ -28,20 +35,14 @@ function New-SqlQuery {
     [CmdletBinding()]
     param (
         [hashtable ]$SqlSettings,
-        [Object] $Object
+        [Object] $Object,
+        $TableMapping
     )
 
     $ArraySQLQueries = New-ArrayList
     if ($Object -ne $null) {
         ## Added fields to know when event was added to SQL and by WHO (in this case TaskS Scheduler User)
         ## Only adding when $Object exists
-        $TableMapping = New-SqlTableMapping -SqlTableMapping $SqlSettings.SqlTableMapping -Object $Object
-        $SQLTable = $SqlSettings.SqlTable
-
-        if ($SqlSettings.SqlTableCreate) {
-            $CreateTableSQL = New-SqlQueryCreateTable -SqlSettings $SqlSettings -Object $Object
-            Add-ToArray -List $ArraySQLQueries -Element $CreateTableSQL
-        }
 
         foreach ($O in $Object) {
             $ArrayMain = New-ArrayList
@@ -62,7 +63,7 @@ function New-SqlQuery {
                     if ($FieldName -eq $MapKey) {
                         $MapValue = $TableMapping.$MapKey
                         if ($FieldValue -is [DateTime]) { $FieldValue = Get-Date $FieldValue -Format "yyyy-MM-dd HH:mm:ss" }
-                        if ($FieldValue -contains "'") { $FieldValue = $FieldValue -Replace "'", "''" }
+                        if ($FieldValue -like "*'*") { $FieldValue = $FieldValue -Replace "'", "''" }
                         #if ($FieldValue -eq '') { $FieldValue = 'NULL' }
                         Add-ToArray -List $ArrayKeys -Element "[$MapValue]"
                         Add-ToArray -List $ArrayValues -Element "'$FieldValue'"
@@ -70,7 +71,7 @@ function New-SqlQuery {
                 }
             }
             if ($ArrayKeys) {
-                Add-ToArray -List $ArrayMain -Element "INSERT INTO $SQLTable ("
+                Add-ToArray -List $ArrayMain -Element "INSERT INTO  $($SqlSettings.SqlTable) ("
                 Add-ToArray -List $ArrayMain -Element ($ArrayKeys -join ',')
                 Add-ToArray -List $ArrayMain -Element ') VALUES ('
                 Add-ToArray -List $ArrayMain -Element ($ArrayValues -join ',')
@@ -91,6 +92,7 @@ function New-SqlTableMapping {
         [Object] $Object
     )
     if ($SqlTableMapping) {
+        #Write-Verbose "Test $($SqlTableMapping.'Record ID')"
         $TableMapping = $SqlTableMapping
     } else {
         $TableMapping = @{}
@@ -112,19 +114,21 @@ function New-SqlTableMapping {
     return $TableMapping
 }
 
+
+
 function New-SqlQueryCreateTable {
     [CmdletBinding()]
     param (
         [hashtable ]$SqlSettings,
-        [Object] $Object
+        [Object] $Object,
+        $TableMapping
     )
 
     $ArraySQLQueries = New-ArrayList
     if ($Object) {
-        $TableMapping = New-SqlTableMapping -SqlTableMapping $SqlSettings.SqlTableMapping -Object $Object
-        $SQLTable = $SqlSettings.SqlTable
 
         foreach ($O in $Object) {
+            #Get-ObjectType -Object $O -Verbose -VerboseOnly
             $ArrayMain = New-ArrayList
             $ArrayKeys = New-ArrayList
             $ArrayValues = New-ArrayList
@@ -133,10 +137,11 @@ function New-SqlQueryCreateTable {
                 $FieldName = $E.Name
                 $FieldValue = $E.Value
 
+                #Write-Verbose "Test1 - Value1: $FieldName Value2: $FieldValue"
                 foreach ($MapKey in $TableMapping.Keys) {
                     if ($FieldName -eq $MapKey) {
                         $MapValue = $TableMapping.$MapKey
-
+                        #Write-Verbose "Test2 - Value1: $FieldName Value2: $FieldValue MapKey: $MapKey"
                         if ($FieldValue -is [DateTime]) {
                             Add-ToArray -List $ArrayKeys -Element "[$MapValue] [DateTime] NULL"
                         } elseif ($FieldValue -is [int] -or $FieldValue -is [Int64]) {
@@ -150,7 +155,7 @@ function New-SqlQueryCreateTable {
                 }
             }
             if ($ArrayKeys) {
-                Add-ToArray -List $ArrayMain -Element "CREATE TABLE $SQLTable ("
+                Add-ToArray -List $ArrayMain -Element "CREATE TABLE $($SqlSettings.SqlTable) ("
                 Add-ToArray -List $ArrayMain -Element "ID int IDENTITY(1,1) PRIMARY KEY,"
                 Add-ToArray -List $ArrayMain -Element ($ArrayKeys -join ',')
 
