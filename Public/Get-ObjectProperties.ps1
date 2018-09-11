a337bb224e84c654996f756420697c854fc29c87
PrzemyslawKlys <pklys@evotec.pl>
2018-09-11 20:18:45 +0200

    Update

--- Public/Get-ObjectProperties.ps1
+++ Public/Get-ObjectProperties.ps1
@@ -1,6 +1,10 @@
+
+# This function goes thru an object such as Get-Aduser and scans every object returned getting all properties
+# This basically makes sure that all properties are known at run time of Export to SQL, Excel or Word
 function Get-ObjectProperties {
     param (
-        [object] $Object
+        [object] $Object,
+        [string[]] $AddProperties # provides ability to add some custom properties
     )
     $Properties = New-ArrayList
     foreach ($O in $Object) {
@@ -8,6 +12,9 @@ function Get-ObjectProperties {
         foreach ($Property in $ObjectProperties) {
             Add-ToArrayAdvanced -List $Properties -Element $Property -SkipNull -RequireUnique
         }
+    }
+    foreach ($Property in $AddProperties) {
+        Add-ToArrayAdvanced -List $Properties -Element $Property -SkipNull -RequireUnique
     }
     return $Properties | Sort-Object
 }
