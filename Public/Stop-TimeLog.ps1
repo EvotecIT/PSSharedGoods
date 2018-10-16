8d42544963a7cd28f607fabb97ccca13107383fb
PrzemyslawKlys <pklys@evotec.pl>
2018-10-16 13:01:19 +0200

    Update

--- Public/Stop-TimeLog.ps1
+++ Public/Stop-TimeLog.ps1
@@ -6,7 +6,7 @@ function Stop-TimeLog {
     )
     Begin {}
     Process {
-        if ($AsArray) {
+        if ($Option -eq 'Array') {
             $TimeToExecute = "$($Time.Elapsed.Days) days", "$($Time.Elapsed.Hours) hours", "$($Time.Elapsed.Minutes) minutes", "$($Time.Elapsed.Seconds) seconds", "$($Time.Elapsed.Milliseconds) milliseconds"
         } else {
             $TimeToExecute = "$($Time.Elapsed.Days) days, $($Time.Elapsed.Hours) hours, $($Time.Elapsed.Minutes) minutes, $($Time.Elapsed.Seconds) seconds, $($Time.Elapsed.Milliseconds) milliseconds"
