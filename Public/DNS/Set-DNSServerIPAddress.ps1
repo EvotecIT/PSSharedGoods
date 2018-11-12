edfb34e3e2856417169d36df27551201e7fa4c71
PrzemyslawKlys <pklys@evotec.pl>
2018-11-12 15:24:26 +0100

    Update

---
+++ Public/DNS/Set-DNSServerIPAddress.ps1
@@ -0,0 +1,24 @@
+<#
+# Where $ServerName can be set as needed
+# Where Service is the name of the Network Card (takes wildcard)
+# Where IpAddresses are given in brackets
+Set-DnsServerIpAddress -ComputerName $ServerName -NicName "Service*" -IpAddresses '8.8.8.8','8.8.4.4','8.8.8.1'
+#>
+
+function Set-DnsServerIpAddress {
+    param(
+        [string] $ComputerName,
+        [string] $NicName,
+        [string] $IpAddresses
+    )
+    if (Test-Connection -ComputerName $ComputerName -Count 2 -Quiet) {
+        Invoke-Command -ComputerName $ComputerName -ScriptBlock { param ($ComputerName, $NicName, $IpAddresses)
+            write-host "Setting on $ComputerName on interface $NicName a new set of DNS Servers $IpAddresses"
+            Set-DnsClientServerAddress -InterfaceAlias $NicName -ServerAddresses $IpAddresses
+
+        } -ArgumentList $ComputerName, $NicName, $IpAddresses
+
+    } else {
+        write-host "Can't access $ComputerName. Computer is not online."
+    }
+}
