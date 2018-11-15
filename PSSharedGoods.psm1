45ff647ddbf510675c6479861feb09adf4d70aa5
PrzemyslawKlys <pklys@evotec.pl>
2018-11-15 22:35:41 +0100

    Update

--- PSSharedGoods.psm1
+++ PSSharedGoods.psm1
@@ -19,7 +19,56 @@ Foreach ($import in @($Assembly)) {
     }
 }
 
-Export-ModuleMember -Function '*'
+$FunctionsToExport = 'Send-EmailNew', 'Connect-Tenant1', 'Add-WinADUserGroups',
+'Get-WinADUserSnapshot', 'Remove-WinADUserGroups',
+'Set-WinADGroupSynchronization', 'Set-WinADUserFields',
+'Set-WinADUserSettingGAL', 'Set-WinADUserStatus',
+'Add-PropertyToList', 'New-UserAdd', 'Set-SpecUser',
+'Set-WinAzureADUserLicense', 'Set-WinAzureADUserStatus',
+'Connect-WinAzure', 'Connect-WinAzureAD', 'Connect-WinExchange',
+'Connect-WinService', 'Connect-WinTeams', 'Disconnect-WinAzure',
+'Disconnect-WinAzureAD', 'Disconnect-WinExchange',
+'Disconnect-WinTeams', 'Get-MyIP', 'Request-Credentials',
+'Convert-ExchangeEmail', 'Convert-ExchangeItems',
+'Convert-ExchangeSize', 'Convert-KeyToKeyValue', 'Convert-Size',
+'Convert-TimeToDays', 'Convert-ToDateTime', 'Convert-ToTimeSpan',
+'Convert-TwoArraysIntoOne', 'Convert-UAC', 'ConvertFrom-SID',
+'ConvertTo-ImmutableID', 'Find-DatesCurrentDayMinusDayX',
+'Find-DatesCurrentDayMinuxDaysX', 'Find-DatesCurrentHour',
+'Find-DatesDayPrevious', 'Find-DatesDayToday',
+'Find-DatesMonthCurrent', 'Find-DatesMonthPast', 'Find-DatesPastHour',
+'Find-DatesPastWeek', 'Find-DatesQuarterCurrent',
+'Find-DatesQuarterLast', 'Set-DnsServerIpAddress', 'Get-HTML',
+'Send-Email', 'Set-EmailBody', 'Set-EmailBodyPreparedTable',
+'Set-EmailBodyReplacement', 'Set-EmailBodyReplacementTable',
+'Set-EmailFormatting', 'Set-EmailHead', 'Set-EmailReportBranding',
+'Set-EmailWordReplacements', 'Set-EmailWordReplacementsHash',
+'Get-FileInformation', 'Get-FilesInFolder', 'Get-FileSize',
+'Get-PathSeparator', 'Get-PathTemporary', 'Add-ToArray',
+'Add-ToArrayAdvanced', 'Add-ToHashTable', 'Format-PSTable',
+'Format-PSTableConvertType1', 'Format-PSTableConvertType2',
+'Format-PSTableConvertType3', 'Format-TransposeTable',
+'Get-HashMaxValue', 'Get-MimeType', 'Get-ObjectCount', 'Get-ObjectData',
+'Get-ObjectKeys', 'Get-ObjectProperties',
+'Get-ObjectPropertiesAdvanced', 'Get-ObjectTitles', 'Get-ObjectType',
+'Get-Types', 'Merge-Objects', 'New-ArrayList',
+'Remove-DuplicateObjects', 'Remove-FromArray',
+'Remove-ObjectsExistingInTarget', 'Rename-UserValuesFromHash',
+'Split-Array', 'Find-MyProgramData', 'Start-MyProgram',
+'Get-RandomCharacters', 'Get-RandomPassword', 'Get-RandomStringName',
+'Get-CommandInfo', 'New-Runspace', 'Start-Runspace', 'Stop-Runspace',
+'Get-SqlQueryColumnInformation', 'New-SqlQuery',
+'New-SqlQueryAlterTable', 'New-SqlQueryCreateTable',
+'New-SqlTableMapping', 'Send-SqlInsert', 'Find-TypesNeeded',
+'Get-ModulesAvailability', 'Search-Command',
+'Test-AvailabilityCommands', 'Test-ComputerAvailability',
+'Test-ConfigurationCredentials', 'Test-ForestConnectivity',
+'Test-Key', 'Test-ModuleAvailability', 'Test-Port', 'Test-WinRM',
+'Get-TimeZoneAdvanced', 'Get-TimeZoneLegacy', 'Start-TimeLog',
+'Stop-TimeLog', 'Show-Array', 'Show-DataInVerbose',
+'Show-TableVisualization', 'Save-XML', 'Set-XML'
+
+Export-ModuleMember -Function $FunctionsToExport
 
 [string] $ManifestFile = '{0}.psd1' -f (Get-Item $PSCommandPath).BaseName;
 $ManifestPathAndFile = Join-Path -Path $PSScriptRoot -ChildPath $ManifestFile;
