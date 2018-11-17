9d5c0e28a69895a11c2947b82a0bd53ed253a16b
PrzemyslawKlys <pklys@evotec.pl>
2018-11-17 20:37:59 +0100

    Update

--- Examples/Format-TableVerbose2.ps1
+++ Examples/Format-TableVerbose2.ps1
@@ -1,3 +1,14 @@
-get-process | Select-Object -First 5 | Format-TableVerbose
-get-process | Select-Object -First 5 | Format-TableVerbose Name, PriorityClass, HandleCount #, FileVersion, Handles, Id, Si , StartInfo, MainModule
-get-process | Select-Object -First 5 | Format-TableVerbose Name, PriorityClass, HandleCount, FileVersion, Handles, Id, Si , StartInfo, MainModule
+get-process | Select-Object -First 5 | Format-Table Name, PriorityClass, HandleCount #-AutoSize
+#get-process | Select-Object -First 5 | Format-TableVerbose Name, PriorityClass, HandleCount #, FileVersion, Handles, Id, Si , StartInfo, MainModule
+get-process | Select-Object -First 5 | Format-TableVerbose Name, PriorityClass, HandleCount , FileVersion, Handles, Id, Si , StartInfo, MainModule
+get-process | Select-Object -First 5 | Format-TableVerbose Id, Si , StartInfo, MainModule
+
+get-process | Select-Object -First 5 | Format-TableVerbose *
+
+get-process | Select-Object -First 5 | Format-Table * # Id, Si , StartInfo, MainModule
+
+#get-aduser -Filter * | Format-TableVerbose
+Format-TableVerbose
+
+
+Format-Table
--- Public/Objects/Format-TableVerbose.ps1
+++ Public/Objects/Format-TableVerbose.ps1
@@ -5,101 +5,104 @@ function Format-TableVerbose {
         [Parameter(Mandatory = $false, ValueFromPipeline = $true, Position = 1)] $InputObject,
         [Parameter(Mandatory = $false, ValueFromPipeline = $false, Position = 0)][Object[]] $Property,
         [Parameter(Mandatory = $false, ValueFromPipeline = $false, Position = 2)][Object[]] $ExcludeProperty,
-        [switch] $Autosize,
         [switch] $HideTableHeaders,
-        [int] $MaximumHeaders = 10
+        [int] $ColumnHeaderSize,
+        [switch] $AlignRight
     )
     Begin {
+        $IsVerbosePresent = $PSCmdlet.MyInvocation.BoundParameters["Verbose"].IsPresent
+
         $VerboseCurrent = $VerbosePreference
         $VerbosePreference = 'continue'
-        $FirstRun = $True
-        $PadRight = 0
-        $PadLeft = 15
-
-        $ScreenWidth = $Host.UI.RawUI.WindowSize.Width - 12
-        #$ScreenHeight = $Host.UI.RawUI.WindowSize.Height
-        #Write-Verbose "Format-TableVerbose - PadLeft: $PadLeft ScreenWidth $ScrenWidth"
+        [bool] $FirstRun = $True # First run for pipeline
+        [bool] $FirstLoop = $True # First loop for data
+        [int] $ScreenWidth = $Host.UI.RawUI.WindowSize.Width - 12 # Removes 12 chars because of VERBOSE: output
+        $ArrayList = @()
     }
     Process {
-        <#
-        if ($Property) {
-            $InputObject = $InputObject | Select-Object -Property $Property
-            $PadLeft = $ScreenWidth / $Property.Count
-        } else {
-            # Properties unknown, Get properties of object
-            $Property = Get-ObjectProperties -Object $InputObject
-            $PropertyCount = $Property.Count
-            if ($PropertyCount -ge $MaximumHeaders) {
-                $PropertyCount = $MaximumHeaders
-            }
-            $Property = ($Property | Select-Object -First $PropertyCount)
-
-            # Prepare Object with just right amount of columns
-            $InputObject = $InputObject | Select-Object -Property $Property
-            $PadLeft = $ScreenWidth / $PropertyCount
-        }
-#>
-
+        if ((Get-ObjectCount -Object $InputObject) -eq 0) { break }
         if ($FirstRun) {
             $FirstRun = $false
             $Data = Format-PSTable -Object $InputObject -Property $Property -ExcludeProperty $ExcludeProperty -NoAliasOrScriptProperties:$NoAliasOrScriptProperties -DisplayPropertySet:$DisplayPropertySet -PreScanHeaders:$PreScanHeaders
             $Headers = $Data[0]
-
-            Write-Color $StringLenghts -Color Red
-
             if ($HideTableHeaders) {
                 $Data.RemoveAt(0);
             }
-
-
-            $i = 0
-            foreach ($RowData in $Data ) {
-                $Output = ''
-                foreach ($Value in $RowData) {
-
-                    $Value = ("$Value".ToCharArray() | Select-Object -first ($PadLeft - 1)) -join ""
-
-                    if ($Output -eq '') {
-                        $Output = "$Value".PadRight($PadRight).PadLeft($PadLeft)
-                    } else {
-                        $Output = $Output + "$Value".PadRight($PadRight).PadLeft($PadLeft)
-                    }
-                }
-                Write-Verbose -Message $Output
-
-                if (-not $HideTableHeaders) {
-                    # Add underline
-                    if ($i -eq 0) {
-                        $HeaderUnderline = $Output -Replace '\w', '-'
-                        Write-Verbose -Message $HeaderUnderline
-                    }
-                }
-
-                $i++
-            }
+            $ArrayList += $Data
         } else {
-
             $Data = Format-PSTable -Object $InputObject -Property $Property -ExcludeProperty $ExcludeProperty -NoAliasOrScriptProperties:$NoAliasOrScriptProperties -DisplayPropertySet:$DisplayPropertySet -PreScanHeaders:$PreScanHeaders -OverwriteHeaders $Headers -SkipTitle
-            foreach ($RowData in $Data) {
-                $Output = ''
-                foreach ($Value in $RowData) {
-                    $Value = ("$Value".ToCharArray() | Select-Object -first ($PadLeft - 1)) -join ""
-
-                    if ($Output -eq '') {
-                        $Output = "$Value".PadRight($PadRight).PadLeft($PadLeft)
-                    } else {
-                        $Output = $Output + "$Value".PadRight($PadRight).PadLeft($PadLeft)
-                    }
-                }
-                Write-Verbose -Message $Output
-            }
+            $ArrayList += $Data
         }
     }
     End {
+        if (-not $ColumnHeaderSize) {
+            $ColumnLength = [int[]]::new($Headers.Count);
+            foreach ($Row in $ArrayList) {
+                $i = 0
+                foreach ($Column in $Row) {
+                    $Length = "$Column".Length
+                    if ($Length -gt $ColumnLength[$i]) {
+                        $ColumnLength[$i] = $Length
+                    }
+                    $i++
+                }
+            }
+            if ($IsVerbosePresent) {
+                Write-Verbose "Format-TableVerbose - ScreenWidth $ScreenWidth"
+                Write-Verbose "Format-TableVerbose - Column Lengths $($ColumnLength -join ',')"
+            }
+        }
+        Write-Verbose -Message ''
+        foreach ($Row in $ArrayList ) {
+            [string] $Output = ''
+            [int] $ColumnNumber = 0
+            [int] $CurrentColumnLength = 0
+            # Prepare each data for row
+            foreach ($ColumnValue in $Row) {
 
+                # Set Column Header Size to static value or based on string length
+                if ($ColumnHeaderSize) {
+                    $PadLength = $ColumnHeaderSize + 1 # Add +1 to make sure there's space between columns
+                } else {
+                    $PadLength = $ColumnLength[$ColumnNumber] + 1 # Add +1 to make sure there's space between columns
+                }
 
+                # Makes sure to display all data on current screen size, the larger the screen, the more it fits
+                $CurrentColumnLength += $PadLength
+                if ($CurrentColumnLength -ge $ScreenWidth) {
+                    break
+                }
 
+                # Prepare Data
+                $ColumnValue = ("$ColumnValue".ToCharArray() | Select-Object -First ($PadLength)) -join ""
+                if ($Output -eq '') {
+                    if ($AlignRight) {
+                        $Output = "$ColumnValue".PadLeft($PadLength)
+                    } else {
+                        $Output = "$ColumnValue".PadRight($PadLength)
+                    }
+                } else {
+                    if ($AlignRight) {
+                        $Output = $Output + "$ColumnValue".PadLeft($PadLength)
+                    } else {
+                        $Output = $Output + "$ColumnValue".PadRight($PadLength)
+                    }
+                }
+                $ColumnNumber++
+            }
+            Write-Verbose -Message $Output
 
+            if (-not $HideTableHeaders) {
+                # Add underline
+                if ($FirstLoop) {
+                    $HeaderUnderline = $Output -Replace '\w', '-'
+                    Write-Verbose -Message $HeaderUnderline
+                }
+            }
+
+            $FirstLoop = $false
+        }
+        Write-Verbose -Message ''
 
         $VerbosePreference = $VerboseCurrent
     }
