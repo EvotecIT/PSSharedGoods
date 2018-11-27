68d815b19f78d93f1ea4eba619706e75a42e891a
PrzemyslawKlys <pklys@evotec.pl>
2018-11-27 19:19:43 +0100

    Logger changes to support empty log path

--- Public/Logging/Get-Logger.ps1
+++ Public/Logging/Get-Logger.ps1
@@ -14,15 +14,16 @@
 function Get-Logger {
     [CmdletBinding()]
     param (
-        [Parameter(Mandatory = $true)]
+        [Parameter(Mandatory = $false)]
         [string] $LogPath,
         [switch] $ShowTime,
         [string] $TimeFormat = 'yyyy-MM-dd HH:mm:ss'
     )
-
-    $LogsDir = [System.IO.Path]::GetDirectoryName($LogPath)
-    New-Item $LogsDir -ItemType Directory -Force | Out-Null
-    New-Item $LogPath -ItemType File -Force | Out-Null
+    if (-not [string]::IsNullOrWhiteSpace($LogPath)) {
+        $LogsDir = [System.IO.Path]::GetDirectoryName($LogPath)
+        New-Item $LogsDir -ItemType Directory -Force | Out-Null
+        New-Item $LogPath -ItemType File -Force | Out-Null
+    }
 
     $Logger = [PSCustomObject]@{
         LogPath    = $LogPath
@@ -35,7 +36,11 @@ function Get-Logger {
             [Parameter(Mandatory = $true)]
             [string]$String
         )
-        Write-Color -Text "[Error] ", $String -Color Red, White -LogFile:$this.LogPath -ShowTime:$this.ShowTime -TimeFormat $this:TimeFormat
+        if ([string]::IsNullOrWhiteSpace($this.LogPath)) {
+            Write-Color -Text "[Error] ", $String -Color Red, White -ShowTime:$this.ShowTime -TimeFormat $this:TimeFormat
+        } else {
+            Write-Color -Text "[Error] ", $String -Color Red, White -LogFile:$this.LogPath -ShowTime:$this.ShowTime -TimeFormat $this:TimeFormat
+        }
     }
 
     Add-Member -InputObject $Logger -MemberType ScriptMethod AddInfoRecord -Value {
@@ -43,7 +48,11 @@ function Get-Logger {
             [Parameter(Mandatory = $true)]
             [string]$String
         )
-        Write-Color -Text "[Info] ", $String -Color Yellow, White -LogFile:$this.LogPath -ShowTime:$this.ShowTime -TimeFormat $this:TimeFormat
+        if ([string]::IsNullOrWhiteSpace($this.LogPath)) {
+            Write-Color -Text "[Info] ", $String -Color Yellow, White -ShowTime:$this.ShowTime -TimeFormat $this:TimeFormat
+        } else {
+            Write-Color -Text "[Info] ", $String -Color Yellow, White -LogFile:$this.LogPath -ShowTime:$this.ShowTime -TimeFormat $this:TimeFormat
+        }
     }
 
     Add-Member -InputObject $Logger -MemberType ScriptMethod AddWarningRecord -Value {
@@ -51,7 +60,11 @@ function Get-Logger {
             [Parameter(Mandatory = $true)]
             [string]$String
         )
-        Write-Color -Text "[Warning] ", $String -Color Magenta, White -LogFile:$this.LogPath -ShowTime:$this.ShowTime -TimeFormat $this:TimeFormat
+        if ([string]::IsNullOrWhiteSpace($this.LogPath)) {
+            Write-Color -Text "[Warning] ", $String -Color Yellow, White -ShowTime:$this.ShowTime -TimeFormat $this:TimeFormat
+        } else {
+            Write-Color -Text "[Warning] ", $String -Color Magenta, White -LogFile:$this.LogPath -ShowTime:$this.ShowTime -TimeFormat $this:TimeFormat
+        }
     }
 
     Add-Member -InputObject $Logger -MemberType ScriptMethod AddRecord -Value {
@@ -59,14 +72,22 @@ function Get-Logger {
             [Parameter(Mandatory = $true)]
             [string]$String
         )
-        Write-Color -Text $String -Color White -LogFile:$this.LogPath -ShowTime:$this.ShowTime -TimeFormat $this:TimeFormat
+        if ([string]::IsNullOrWhiteSpace($this.LogPath)) {
+            Write-Color -Text $String -Color Yellow, White -ShowTime:$this.ShowTime -TimeFormat $this:TimeFormat
+        } else {
+            Write-Color -Text $String -Color White -LogFile:$this.LogPath -ShowTime:$this.ShowTime -TimeFormat $this:TimeFormat
+        }
     }
     Add-Member -InputObject $Logger -MemberType ScriptMethod AddSuccessRecord -Value {
         param(
             [Parameter(Mandatory = $true)]
             [string]$String
         )
-        Write-Color -Text "[Success] ", $String -Color Green, White -LogFile:$this.LogPath -ShowTime:$this.ShowTime -TimeFormat $this:TimeFormat
+        if ([string]::IsNullOrWhiteSpace($this.LogPath)) {
+            Write-Color -Text "[Success] ", $String -Color Yellow, White -ShowTime:$this.ShowTime -TimeFormat $this:TimeFormat
+        } else {
+            Write-Color -Text "[Success] ", $String -Color Green, White -LogFile:$this.LogPath -ShowTime:$this.ShowTime -TimeFormat $this:TimeFormat
+        }
     }
     return $Logger
 }
