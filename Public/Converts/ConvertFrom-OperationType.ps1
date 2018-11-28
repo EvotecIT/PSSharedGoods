580fcd088f5cdc73c1b8eb72e315eae1a8485d9e
PrzemyslawKlys <pklys@evotec.pl>
2018-11-29 00:36:46 +0100

    Moved from PSWinReporting

---
+++ Public/Converts/ConvertFrom-OperationType.ps1
@@ -0,0 +1,14 @@
+Function ConvertFrom-OperationType {
+    param (
+        [string] $OperationType
+    )
+    $Known = @{
+        '%%14674' = 'Value Added'
+        '%%14675' = 'Value Deleted'
+        '%%14676' = 'Unknown'
+    }
+    foreach ($id in $OperationType) {
+        if ($name = $Known[$id]) { return $name }
+    }
+    return $OperationType
+}
