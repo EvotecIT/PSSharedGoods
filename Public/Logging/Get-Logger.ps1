df995251504cff111bed986f71d97a3aa040672e
Anatoly Demidko <a.demidko@compassplus.com>
2018-12-05 17:25:55 +0500

    Improve logging func

--- Public/Logging/Get-Logger.ps1
+++ Public/Logging/Get-Logger.ps1
@@ -3,23 +3,47 @@
     .SYNOPSIS
     Returns an instance of the logger object.
 
-    .EXAMPLE
+    .EXAMPLE with full log name
     $Logger = Get-Logger -ShowTime -LogPath 'C:\temp\test.log'
     $Logger.AddErrorRecord("test error")
     $Logger.AddInfoRecord("test info")
     $Logger.AddSuccessRecord("test success")
     $Logger.AddRecord("test record")
+
+    .EXAMPLE with directory name and auto-generated log name
+    $Logger = Get-Logger -ShowTime -LogsDir 'C:\temp'
+    $Logger.AddErrorRecord("test error")
+
+    .EXAMPLE with directory name and logo name defined separately
+    $Logger = Get-Logger -ShowTime -Directory 'C:\temp' -Filename 'test.log'
+    $Logger.AddErrorRecord("test error")
+
+    .EXAMPLE without logfile, only console output
+    $Logger = Get-Logger -ShowTime
+    $Logger.AddErrorRecord("test error")
 #>
 
 function Get-Logger {
-    [CmdletBinding()]
+    [CmdletBinding(DefaultParameterSetName="All")]
     param (
-        [Parameter(Mandatory = $false)]
+        [Parameter(Mandatory = $false, ParameterSetName = 'Logpath')]
         [string] $LogPath,
+        [Parameter(Mandatory = $false, ParameterSetName = 'Complexpath')]
+        [string] $LogsDir,
+        [Parameter(Mandatory = $false, ParameterSetName = 'Complexpath')]
+        [string] $Filename,
         [switch] $ShowTime,
         [string] $TimeFormat = 'yyyy-MM-dd HH:mm:ss'
     )
-    if (-not [string]::IsNullOrWhiteSpace($LogPath)) {
+
+    if ($PSCmdlet.ParameterSetName -eq 'Complexpath') {
+        if (-not $Filename) {
+            $Filename = "$([datetime]::Now.ToString($TimeFormat) -replace('[^.\-\w]', '_'))_ADReporting.log"
+        }
+        $LogPath = Join-Path $LogsDir $Filename
+    }
+
+    if ($LogPath) {
         $LogsDir = [System.IO.Path]::GetDirectoryName($LogPath)
         New-Item $LogsDir -ItemType Directory -Force | Out-Null
         New-Item $LogPath -ItemType File -Force | Out-Null
@@ -36,7 +60,7 @@ function Get-Logger {
             [Parameter(Mandatory = $true)]
             [string]$String
         )
-        if ([string]::IsNullOrWhiteSpace($this.LogPath)) {
+        if (-not $this.LogPath) {
             Write-Color -Text "[Error] ", $String -Color Red, White -ShowTime:$this.ShowTime -TimeFormat $this:TimeFormat
         } else {
             Write-Color -Text "[Error] ", $String -Color Red, White -LogFile:$this.LogPath -ShowTime:$this.ShowTime -TimeFormat $this:TimeFormat
@@ -48,7 +72,7 @@ function Get-Logger {
             [Parameter(Mandatory = $true)]
             [string]$String
         )
-        if ([string]::IsNullOrWhiteSpace($this.LogPath)) {
+        if (-not $this.LogPath) {
             Write-Color -Text "[Info] ", $String -Color Yellow, White -ShowTime:$this.ShowTime -TimeFormat $this:TimeFormat
         } else {
             Write-Color -Text "[Info] ", $String -Color Yellow, White -LogFile:$this.LogPath -ShowTime:$this.ShowTime -TimeFormat $this:TimeFormat
@@ -60,7 +84,7 @@ function Get-Logger {
             [Parameter(Mandatory = $true)]
             [string]$String
         )
-        if ([string]::IsNullOrWhiteSpace($this.LogPath)) {
+        if (-not $this.LogPath) {
             Write-Color -Text "[Warning] ", $String -Color Magenta, White -ShowTime:$this.ShowTime -TimeFormat $this:TimeFormat
         } else {
             Write-Color -Text "[Warning] ", $String -Color Magenta, White -LogFile:$this.LogPath -ShowTime:$this.ShowTime -TimeFormat $this:TimeFormat
@@ -72,7 +96,7 @@ function Get-Logger {
             [Parameter(Mandatory = $true)]
             [string]$String
         )
-        if ([string]::IsNullOrWhiteSpace($this.LogPath)) {
+        if (-not $this.LogPath) {
             Write-Color -Text " $String" -Color White -ShowTime:$this.ShowTime -TimeFormat $this:TimeFormat
         } else {
             Write-Color -Text " $String" -Color White -LogFile:$this.LogPath -ShowTime:$this.ShowTime -TimeFormat $this:TimeFormat
@@ -83,7 +107,7 @@ function Get-Logger {
             [Parameter(Mandatory = $true)]
             [string]$String
         )
-        if ([string]::IsNullOrWhiteSpace($this.LogPath)) {
+        if (-not $this.LogPath) {
             Write-Color -Text "[Success] ", $String -Color Green, White -ShowTime:$this.ShowTime -TimeFormat $this:TimeFormat
         } else {
             Write-Color -Text "[Success] ", $String -Color Green, White -LogFile:$this.LogPath -ShowTime:$this.ShowTime -TimeFormat $this:TimeFormat
