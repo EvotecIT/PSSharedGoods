26c48650ae0ef73c9f414bf9a147bff17cdcb84d
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2019-04-26 11:49:27 +0200

    Update

--- Public/Programs/Start-MyProgram.ps1
+++ Public/Programs/Start-MyProgram.ps1
@@ -2,7 +2,16 @@ function Start-MyProgram {
     [CmdletBinding()]
     param (
         [string] $Program,
-        [string[]] $CmdArgList
+        [string[]] $CmdArgList,
+        [System.Collections.IDictionary] $LoggerParameters
     )
-    return & $Program $CmdArgList
+
+   # $Output = & $Program $CmdArgList 2>&1
+    $Output = (cmd /c $Program $CmdArgList '2>&1')
+    if (-not $LoggerParameters) {
+        return $Output
+    } else {
+        $Logger = Get-Logger @LoggerParameters
+        $Logger.AddInfoRecord("Running program $Program - $Output")
+    }
 }
