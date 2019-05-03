ec0aac6fe5661c3558ac2a5aefc04a9b7c43c767
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2019-05-03 21:31:38 +0200

    Update

--- Public/TestFunctionality/Test-ConfigurationCredentials.ps1
+++ Public/TestFunctionality/Test-ConfigurationCredentials.ps1
@@ -4,11 +4,10 @@ function Test-ConfigurationCredentials {
         [Object] $Configuration,
         $AllowEmptyKeys
     )
-    $Object = @()
-    foreach ($Key in $Configuration.Keys) {
+    $Object = foreach ($Key in $Configuration.Keys) {
         if ($AllowEmptyKeys -notcontains $Key -and [string]::IsNullOrWhiteSpace($Configuration.$Key)) {
             Write-Verbose "Test-ConfigurationCredentials - Configuration $Key is Null or Empty! Terminating"
-            $Object += @{ Status = $false; Output = $User.SamAccountName; Extended = "Credentials configuration $Key is Null or Empty!" }
+            @{ Status = $false; Output = $User.SamAccountName; Extended = "Credentials configuration $Key is Null or Empty!" }
         }
     }
     return $Object
