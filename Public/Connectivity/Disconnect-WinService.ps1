77b02367b7ffa7ecd88c28bb4f3b82d0aff5160e
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2019-06-13 23:35:14 +0200

    Moved to Connectimo

--- Public/Connectivity/Disconnect-WinService.ps1
+++
@@ -1,7 +0,0 @@
-function Disconnect-WinService {
-    [CmdletBinding()]
-    param(
-
-    )
-    Get-PSSession | Remove-PSSession
-}
--- Public/Programs/Test-InstalledApplication.ps1
+++
@@ -1,8 +0,0 @@
-Function Test-InstalledApplication {
-    [CmdletBinding()]
-    Param(
-        [alias('ApplicationName')] [string] $DisplayName
-    )
-    $App = Get-InstalledApplication -DisplayName $DisplayName -Type UserInstalled
-    return $App
-}
