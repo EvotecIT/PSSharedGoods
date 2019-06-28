0a366a7820fceba4f21e5562ad94ededafb255a6
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2019-06-28 14:49:43 +0200

    Update

---
+++ Public/Objects/Remove-EmptyValues.ps1
@@ -0,0 +1,11 @@
+﻿function Remove-EmptyValues {
+    [CmdletBinding()]
+    param(
+        [System.Collections.IDictionary] $Hashtable
+    )
+    foreach ($_ in [string[]] $Hashtable.Keys) {
+        if ($null -eq $Hashtable[$_] -or $Hashtable[$_] -eq '') {
+            $Hashtable.Remove($_)
+        }
+    }
+}
