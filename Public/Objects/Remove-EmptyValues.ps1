26c1062fa0304e27201490c7b9171ea4303c306a
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2019-09-28 13:56:13 +0200

    Update

--- Public/Objects/Remove-EmptyValues.ps1
+++ Public/Objects/Remove-EmptyValues.ps1
@@ -1,11 +1,37 @@
 ﻿function Remove-EmptyValues {
     [CmdletBinding()]
     param(
-        [System.Collections.IDictionary] $Hashtable
+        [System.Collections.IDictionary] $Hashtable,
+        [switch] $Recursive,
+        [int] $Rerun
     )
     foreach ($_ in [string[]] $Hashtable.Keys) {
-        if ($null -eq $Hashtable[$_] -or $Hashtable[$_] -eq '') {
-            $Hashtable.Remove($_)
+        if ($Recursive) {
+            if ($Hashtable[$_] -is [System.Collections.IDictionary]) {
+
+                if ($Hashtable[$_].Count -eq 0) {
+                    $Hashtable.Remove($_)
+                } else {
+                    Remove-EmptyValues -Hashtable $Hashtable[$_] -Recursive:$Recursive
+                }
+            } else {
+                if ($null -eq $Hashtable[$_]) {
+                    $Hashtable.Remove($_)
+                } elseif ($Hashtable[$_] -is [string] -and $Hashtable[$_] -eq '') {
+                    $Hashtable.Remove($_)
+                }
+            }
+        } else {
+            if ($null -eq $Hashtable[$_]) {
+                $Hashtable.Remove($_)
+            } elseif ($Hashtable[$_] -is [string] -and $Hashtable[$_] -eq '') {
+                $Hashtable.Remove($_)
+            }
+        }
+    }
+    if ($Rerun) {
+        for ($i = 0; $i -lt $Rerun; $i++) {
+            Remove-EmptyValues -Hashtable $Hashtable -Recursive:$Recursive
         }
     }
 }
