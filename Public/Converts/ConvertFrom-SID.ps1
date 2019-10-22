9c4edcd10eb2880ab9e2084bd979b2192da7b864
Przemysław Klys <JZ5Q@eurofins.com>
2019-10-22 13:22:43 +0200

    Update

--- Public/Converts/ConvertFrom-SID.ps1
+++ Public/Converts/ConvertFrom-SID.ps1
@@ -1,39 +1,43 @@
-function ConvertFrom-SID ($Sid) {
-    $KnownSIDs = @{
-        'S-1-0' = 'Null Authority'
-        'S-1-0-0' = 'Nobody'
-        'S-1-1' = 'World Authority'
-        'S-1-1-0' = 'Everyone'
-        'S-1-2' = 'Local Authority'
-        'S-1-2-0' = 'Local'
-        'S-1-2-1' = 'Console Logon'
-        'S-1-3' = 'Creator Authority'
-        'S-1-3-0' = 'Creator Owner'
-        'S-1-3-1' = 'Creator Group'
-        'S-1-3-2' = 'Creator Owner Server'
-        'S-1-3-3' = 'Creator Group Server'
-        'S-1-3-4' = 'Owner Rights'
-        'S-1-5-80-0' = 'All Services'
-        'S-1-4' = 'Non-unique Authority'
-        'S-1-5' = 'NT Authority'
-        'S-1-5-1' = 'Dialup'
-        'S-1-5-2' = 'Network'
-        'S-1-5-3' = 'Batch'
-        'S-1-5-4' = 'Interactive'
-        'S-1-5-6' = 'Service'
-        'S-1-5-7' = 'Anonymous'
-        'S-1-5-8' = 'Proxy'
-        'S-1-5-9' = 'Enterprise Domain Controllers'
-        'S-1-5-10' = 'Principal Self'
-        'S-1-5-11' = 'Authenticated Users'
-        'S-1-5-12' = 'Restricted Code'
-        'S-1-5-13' = 'Terminal Server Users'
-        'S-1-5-14' = 'Remote Interactive Logon'
-        'S-1-5-15' = 'This Organization'
-        'S-1-5-17' = 'This Organization'
-        'S-1-5-18' = 'Local System'
-        'S-1-5-19' = 'NT Authority'
-        'S-1-5-20' = 'NT Authority'
+function ConvertFrom-SID {
+    [cmdletbinding()]
+    param(
+        [string[]] $SID
+    )
+    $wellKnownSIDs = @{
+        'S-1-0'        = 'Null Authority'
+        'S-1-0-0'      = 'Nobody'
+        'S-1-1'        = 'World Authority'
+        'S-1-1-0'      = 'Everyone'
+        'S-1-2'        = 'Local Authority'
+        'S-1-2-0'      = 'Local'
+        'S-1-2-1'      = 'Console Logon'
+        'S-1-3'        = 'Creator Authority'
+        'S-1-3-0'      = 'Creator Owner'
+        'S-1-3-1'      = 'Creator Group'
+        'S-1-3-2'      = 'Creator Owner Server'
+        'S-1-3-3'      = 'Creator Group Server'
+        'S-1-3-4'      = 'Owner Rights'
+        'S-1-5-80-0'   = 'All Services'
+        'S-1-4'        = 'Non-unique Authority'
+        'S-1-5'        = 'NT Authority'
+        'S-1-5-1'      = 'Dialup'
+        'S-1-5-2'      = 'Network'
+        'S-1-5-3'      = 'Batch'
+        'S-1-5-4'      = 'Interactive'
+        'S-1-5-6'      = 'Service'
+        'S-1-5-7'      = 'Anonymous'
+        'S-1-5-8'      = 'Proxy'
+        'S-1-5-9'      = 'Enterprise Domain Controllers'
+        'S-1-5-10'     = 'Principal Self'
+        'S-1-5-11'     = 'Authenticated Users'
+        'S-1-5-12'     = 'Restricted Code'
+        'S-1-5-13'     = 'Terminal Server Users'
+        'S-1-5-14'     = 'Remote Interactive Logon'
+        'S-1-5-15'     = 'This Organization'
+        'S-1-5-17'     = 'This Organization'
+        'S-1-5-18'     = 'Local System'
+        'S-1-5-19'     = 'NT Authority'
+        'S-1-5-20'     = 'NT Authority'
         'S-1-5-32-544' = 'Administrators'
         'S-1-5-32-545' = 'Users'
         'S-1-5-32-546' = 'Guests'
@@ -43,15 +47,15 @@ function ConvertFrom-SID ($Sid) {
         'S-1-5-32-550' = 'Print Operators'
         'S-1-5-32-551' = 'Backup Operators'
         'S-1-5-32-552' = 'Replicators'
-        'S-1-5-64-10' = 'NTLM Authentication'
-        'S-1-5-64-14' = 'SChannel Authentication'
-        'S-1-5-64-21' = 'Digest Authority'
-        'S-1-5-80' = 'NT Service'
-        'S-1-5-83-0' = 'NT VIRTUAL MACHINE\Virtual Machines'
-        'S-1-16-0' = 'Untrusted Mandatory Level'
-        'S-1-16-4096' = 'Low Mandatory Level'
-        'S-1-16-8192' = 'Medium Mandatory Level'
-        'S-1-16-8448' = 'Medium Plus Mandatory Level'
+        'S-1-5-64-10'  = 'NTLM Authentication'
+        'S-1-5-64-14'  = 'SChannel Authentication'
+        'S-1-5-64-21'  = 'Digest Authority'
+        'S-1-5-80'     = 'NT Service'
+        'S-1-5-83-0'   = 'NT VIRTUAL MACHINE\Virtual Machines'
+        'S-1-16-0'     = 'Untrusted Mandatory Level'
+        'S-1-16-4096'  = 'Low Mandatory Level'
+        'S-1-16-8192'  = 'Medium Mandatory Level'
+        'S-1-16-8448'  = 'Medium Plus Mandatory Level'
         'S-1-16-12288' = 'High Mandatory Level'
         'S-1-16-16384' = 'System Mandatory Level'
         'S-1-16-20480' = 'Protected Process Mandatory Level'
@@ -75,21 +79,25 @@ function ConvertFrom-SID ($Sid) {
         'S-1-5-32-579' = 'BUILTIN\Access Control Assistance Operators'
         'S-1-5-32-580' = 'BUILTIN\Remote Management Users'
     }
-    foreach ($id in $sid) {
-        if ($name = $KnownSIDs[$id]) { }
-        else {
-            #Try to translate the SID to an account
-            Try {
-                $objSID = New-Object System.Security.Principal.SecurityIdentifier($id)
-                $name = ( $objSID.Translate([System.Security.Principal.NTAccount]) ).Value
-            } Catch {
-                $name = $sid # returns sid if unable to name
+    foreach ($_ in $SID) {
+        if ($wellKnownSIDs[$_]) {
+            [PSCustomObject] @{
+                Name = $wellKnownSIDs[$_]
+                SID  = $_
+            }
+        } else {
+            try {
+                [PSCustomObject] @{
+                    Name = (([System.Security.Principal.SecurityIdentifier]::new($_)).Translate([System.Security.Principal.NTAccount])).Value
+                    SID  = $_
+                }
+            } catch {
+                # Return unchanged object
+                [PSCustomObject] @{
+                    Name = $_
+                    SID  = $_
+                }
             }
         }
-        return @{ SID = $id
-            Name = $name
-        }
-
     }
-
 }
