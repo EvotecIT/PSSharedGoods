b442dcf53b72537a2934bd1b8cb3b77021025f85
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2019-11-11 13:17:29 +0100

    Update

---
+++ Public/Objects/Get-Colors.ps1
@@ -0,0 +1,14 @@
+﻿function Get-Colors {
+    [CmdletBinding()]
+    param(
+        [string[]] $Color
+    )
+    if ($Color) {
+        foreach ($_ in $Color) {
+            $Script:RGBColors.$_
+        }
+    } else {
+        return $Script:RGBColors
+    }
+}
+Register-ArgumentCompleter -CommandName Get-Colors -ParameterName Color -ScriptBlock { $Script:RGBColors.Keys }
