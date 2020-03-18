29af04f45c16ffb3fdaeb0c9c156cb5a9566cf50
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2020-03-18 21:13:33 +0100

    Improved copy of ordered hashtable

---
+++ Public/Objects/Copy-Dictionary.ps1
@@ -0,0 +1,15 @@
+﻿function Copy-Dictionary {
+    [alias('Copy-Hashtable','Copy-OrderedHashtable')]
+    [cmdletbinding()]
+    param(
+        [System.Collections.IDictionary] $Dictionary
+    )
+    # create a deep-clone of an object
+    $ms = [System.IO.MemoryStream]::new()
+    $bf = [System.Runtime.Serialization.Formatters.Binary.BinaryFormatter]::new()
+    $bf.Serialize($ms, $Dictionary)
+    $ms.Position = 0
+    $clone = $bf.Deserialize($ms)
+    $ms.Close()
+    $clone
+}
