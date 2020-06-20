a5bd0b6ec123e198a0d6f0bc1737243f9b363b56
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2020-06-20 13:54:11 +0200

    Get-FileMetaData added

---
+++ Public/FilesFolders/Get-FileMetaData.ps1
@@ -0,0 +1,85 @@
+﻿function Get-FileMetaData {
+    <#
+    .SYNOPSIS
+    Small function that gets metadata information from file providing similar output to what Explorer shows when viewing file
+
+    .DESCRIPTION
+    Small function that gets metadata information from file providing similar output to what Explorer shows when viewing file
+
+    .PARAMETER File
+    FileName or FileObject
+
+    .EXAMPLE
+    Get-ChildItem -Path $Env:USERPROFILE\Desktop -Force | Get-FileMetaData | Out-HtmlView -ScrollX -Filtering -AllProperties
+
+    .EXAMPLE
+    Get-ChildItem -Path $Env:USERPROFILE\Desktop -Force | Where-Object { $_.Attributes -like '*Hidden*' } | Get-FileMetaData | Out-HtmlView -ScrollX -Filtering -AllProperties
+
+    .NOTES
+    #>
+    [CmdletBinding()]
+    param (
+        [Parameter(Position = 0, ValueFromPipeline)][Object] $File
+    )
+    Begin {
+        $MetaDataObject = [ordered] @{}
+    }
+    Process {
+        foreach ($F in $File) {
+            if ($F -is [string]) {
+                $FileInformation = Get-ItemProperty -Path $F
+            } elseif ($F -is [System.IO.DirectoryInfo]) {
+                Write-Warning "Get-FileMetaData - Directories are not supported. Skipping $F."
+                continue
+            } elseif ($F -is [System.IO.FileInfo]) {
+                $FileInformation = $F
+            } else {
+                Write-Warning "Get-FileMetaData - Only files are supported. Skipping $F."
+                continue
+            }
+            $ShellApplication = New-Object -ComObject Shell.Application
+            $ShellFolder = $ShellApplication.Namespace($FileInformation.Directory.FullName)
+            $ShellFile = $ShellFolder.ParseName($FileInformation.Name)
+            $MetaDataProperties = 0..287 | ForEach-Object -Process {
+                '{0} = {1}' -f $_, $ShellFolder.GetDetailsOf($null, $_)
+            }
+            For ($i = 0; $i -le 287; $i++) {
+                $Property = ($MetaDataProperties[$i].split("="))[1].Trim()
+                $Property = (Get-Culture).TextInfo.ToTitleCase($Property).Replace(' ', '')
+                $Value = $ShellFolder.GetDetailsOf($ShellFile, $i)
+                if ($Property -in 'Attributes', 'Folder', 'Type', 'SpaceFree', 'TotalSize', 'SpaceUsed') {
+                    continue
+                }
+                If (($null -ne $Value) -and ($Value -ne '')) {
+                    $MetaDataObject["$Property"] = $Value
+                }
+            }
+            if ($FileInformation.VersionInfo) {
+                $SplitInfo = ([string] $FileInformation.VersionInfo).Split([char]13)
+                foreach ($Item in $SplitInfo) {
+                    $Property = $Item.Split(":").Trim()
+                    if ($Property[0]) {
+                        $MetaDataObject["$($Property[0])"] = $Property[1]
+                    }
+                }
+            }
+            $MetaDataObject["Attributes"] = $FileInformation.Attributes
+            $MetaDataObject['IsReadOnly'] = $FileInformation.IsReadOnly
+            $MetaDataObject['IsHidden'] = $FileInformation.Attributes -like '*Hidden*'
+            $MetaDataObject['IsSystem'] = $FileInformation.Attributes -like '*System*'
+
+            $DigitalSignature = Get-AuthenticodeSignature -FilePath $FileInformation.Fullname
+            $MetaDataObject['SignatureCertificateSubject'] = $DigitalSignature.SignerCertificate.Subject
+            $MetaDataObject['SignatureCertificateIssuer'] = $DigitalSignature.SignerCertificate.Issuer
+            $MetaDataObject['SignatureCertificateSerialNumber'] = $DigitalSignature.SignerCertificate.SerialNumber
+            $MetaDataObject['SignatureCertificateNotBefore'] = $DigitalSignature.SignerCertificate.NotBefore
+            $MetaDataObject['SignatureCertificateNotAfter'] = $DigitalSignature.SignerCertificate.NotAfter
+            $MetaDataObject['SignatureCertificateThumbprint'] = $DigitalSignature.SignerCertificate.Thumbprint
+            $MetaDataObject['SignatureStatus'] = $DigitalSignature.Status
+            $MetaDataObject['IsOSBinary'] = $DigitalSignature.IsOSBinary
+            [PSCustomObject] $MetaDataObject
+        }
+    }
+}
+#Get-ChildItem -Path $Env:USERPROFILE\Desktop -Force | Get-FileMetaData | Out-HtmlView -ScrollX -Filtering -AllProperties
+#Get-ChildItem -Path $Env:USERPROFILE\Desktop -Force | Where-Object { $_.Attributes -like '*Hidden*' } | Get-FileMetaData | Out-HtmlView -ScrollX -Filtering -AllProperties
