02d75287410fb676ffc467515f0d6d85490ea294
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2020-07-31 09:33:03 +0200

    Update to Format-ToTitleCase

--- Public/Objects/Format-ToTitleCase.ps1
+++ Public/Objects/Format-ToTitleCase.ps1
@@ -1,21 +1,59 @@
-
-<#
-Format-ToTitleCase 'me'
-
-'me i feel good' | Format-ToTitleCase
-
-'me i feel', 'not feel' |Format-ToTitleCase
-#>
-
 function Format-ToTitleCase {
+    <#
+    .SYNOPSIS
+    Formats string or number of strings to Title Case
+
+    .DESCRIPTION
+    Formats string or number of strings to Title Case allowing for prettty display
+
+    .PARAMETER Text
+    Sentence or multiple sentences to format
+
+    .PARAMETER RemoveWhiteSpace
+    Removes spaces after formatting string to Title Case.
+
+    .EXAMPLE
+    Format-ToTitleCase 'me'
+
+    Output:
+    Me
+
+    .EXAMPLE
+    'me i feel good' | Format-ToTitleCase
+
+    Output:
+    Me I Feel Good
+    Not Feel
+
+    .EXAMPLE
+    'me i feel', 'not feel' | Format-ToTitleCase
+
+    Output:
+    Me I Feel Good
+    Not Feel
+
+    .EXAMPLE
+    Format-ToTitleCase -Text 'This is my thing' -RemoveWhiteSpace
+
+    Output:
+    ThisIsMyThing
+
+    .NOTES
+    General notes
+    #>
     [CmdletBinding()]
     param(
-        [Parameter(Mandatory = $true, ValueFromPipeline = $true, Position = 0)][string[]] $Text
+        [Parameter(Mandatory = $true, ValueFromPipeline = $true, Position = 0)][string[]] $Text,
+        [switch] $RemoveWhiteSpace
     )
     Begin {}
     Process {
         $Conversion = foreach ($T in $Text) {
-            (Get-Culture).TextInfo.ToTitleCase($T)
+            if ($RemoveWhiteSpace) {
+                (Get-Culture).TextInfo.ToTitleCase($T) -replace ' ', ''
+            } else {
+                (Get-Culture).TextInfo.ToTitleCase($T)
+            }
         }
     }
     End {
