68c79ee32653dac1d53dd6496bacfd95f50ea19b
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2020-08-30 08:46:49 +0200

    Update

---
+++ Public/Objects/Test-IsDistinguishedName.ps1
@@ -0,0 +1,29 @@
+﻿function Test-IsDistinguishedName {
+    <#
+    .SYNOPSIS
+    Short description
+
+    .DESCRIPTION
+    Long description
+
+    .PARAMETER Identity
+    Parameter description
+
+    .EXAMPLE
+    An example
+
+    .NOTES
+    Original source: https://github.com/PsCustomObject/IT-ToolBox/blob/master/Public/Test-IsValidDn.ps1
+
+    #>
+    [alias('Test-IsDN')]
+    [cmdletBinding()]
+    param (
+        [Parameter(Mandatory, ValueFromPipeline, ValueFromPipelineByPropertyName)]
+        [Alias('DN', 'DistinguishedName')][string] $Identity
+    )
+    Process {
+        [regex]$distinguishedNameRegex = '^(?:(?<cn>CN=(?<name>(?:[^,]|\,)*)),)?(?:(?<path>(?:(?:CN|OU)=(?:[^,]|\,)+,?)+),)?(?<domain>(?:DC=(?:[^,]|\,)+,?)+)$'
+        $Identity -match $distinguishedNameRegex
+    }
+}
---
+++ Tests/Test-IsDistinguishedName.Tests.ps1
@@ -0,0 +1,19 @@
+﻿Describe 'Test-IsDistinguishedName' {
+    It 'Direct input' {
+        Test-IsDistinguishedName -Identity 'CN=Domain Admins,CN=Users,DC=ad,DC=evotec,DC=pl' | Should -Be $true
+        Test-IsDistinguishedName -Identity 'CN=Domain Admins,CN=Users,DC=ad,DC=evotec,DC=pl' | Should -Be $true
+        Test-IsDistinguishedName -Identity 'ad.evotec.pl' | Should -Be $false
+        Test-IsDistinguishedName -Identity 'test.evotec.pl' | Should -Be $false
+        Test-IsDistinguishedName -Identity 'NT AUTHORITY\INTERACTIVE' | Should -Be $false
+        Test-IsDistinguishedName -Identity 'CN=Domain Admins,CN=Users,DC=ad,DC=evotec,DC=pl' | Should -Be $true
+
+    }
+    It 'Pipeline input' {
+        'CN=Domain Admins,CN=Users,DC=ad,DC=evotec,DC=pl' | Test-IsDistinguishedName | Should -Be $true
+        'CN=Domain Admins,CN=Users,DC=ad,DC=evotec,DC=pl' | Test-IsDistinguishedName | Should -Be $true
+        'ad.evotec.pl' | Test-IsDistinguishedName | Should -Be $false
+        'test.evotec.pl' | Test-IsDistinguishedName | Should -Be $false
+        'NT AUTHORITY\INTERACTIVE' | Test-IsDistinguishedName | Should -Be $false
+        'CN=Domain Admins,CN=Users,DC=ad,DC=evotec,DC=pl' | Test-IsDistinguishedName | Should -Be $true
+    }
+}
