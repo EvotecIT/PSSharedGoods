3aedf9b64a8d02f08d2ac175d32ff3881b288855
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2020-09-05 21:17:03 +0200

    Update

--- Public/Email/Set-EmailHead.ps1
+++ Public/Email/Set-EmailHead.ps1
@@ -1,74 +1,79 @@
 function Set-EmailHead {
+    [cmdletBinding()]
     param(
         [System.Collections.IDictionary] $FormattingOptions
     )
     $head = @"
-    <head>
-        <style>
-        BODY {
-            background-color: white;
-            font-family: $($FormattingOptions.FontFamily);
-            font-size: $($FormattingOptions.FontSize);
-        }
+<!DOCTYPE html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<meta content="width=device-width, initial-scale=1" name="viewport">
+<meta name="description" content="Password Expiration Email">
+    <style>
+    BODY {
+        background-color: white;
+        font-family: $($FormattingOptions.FontFamily);
+        font-size: $($FormattingOptions.FontSize);
+    }
 
-        TABLE {
-            border-width: 1px;
-            border-style: solid;
-            border-color: black;
-            border-collapse: collapse;
-            font-family: $($FormattingOptions.FontTableDataFamily);
-            font-size: $($FormattingOptions.FontTableDataSize);
-        }
+    TABLE {
+        border-width: 1px;
+        border-style: solid;
+        border-color: black;
+        border-collapse: collapse;
+        font-family: $($FormattingOptions.FontTableDataFamily);
+        font-size: $($FormattingOptions.FontTableDataSize);
+    }
 
-        TH {
-            border-width: 1px;
-            padding: 3px;
-            border-style: solid;
-            border-color: black;
-            background-color: #00297A;
-            color: white;
-            font-family: $($FormattingOptions.FontTableHeadingFamily);
-            font-size: $($FormattingOptions.FontTableHeadingSize);
-        }
-        TR {
-            font-family: $($FormattingOptions.FontTableDataFamily);
-            font-size: $($FormattingOptions.FontTableDataSize);
-        }
+    TH {
+        border-width: 1px;
+        padding: 3px;
+        border-style: solid;
+        border-color: black;
+        background-color: #00297A;
+        color: white;
+        font-family: $($FormattingOptions.FontTableHeadingFamily);
+        font-size: $($FormattingOptions.FontTableHeadingSize);
+    }
+    TR {
+        font-family: $($FormattingOptions.FontTableDataFamily);
+        font-size: $($FormattingOptions.FontTableDataSize);
+    }
 
-        UL {
-            font-family: $($FormattingOptions.FontFamily);
-            font-size: $($FormattingOptions.FontSize);
-        }
+    UL {
+        font-family: $($FormattingOptions.FontFamily);
+        font-size: $($FormattingOptions.FontSize);
+    }
 
-        LI {
-            font-family: $($FormattingOptions.FontFamily);
-            font-size: $($FormattingOptions.FontSize);
-        }
+    LI {
+        font-family: $($FormattingOptions.FontFamily);
+        font-size: $($FormattingOptions.FontSize);
+    }
 
-        TD {
-            border-width: 1px;
-            padding-right: 2px;
-            padding-left: 2px;
-            padding-top: 0px;
-            padding-bottom: 0px;
-            border-style: solid;
-            border-color: black;
-            background-color: white;
-            font-family: $($FormattingOptions.FontTableDataFamily);
-            font-size: $($FormattingOptions.FontTableDataSize);
-        }
+    TD {
+        border-width: 1px;
+        padding-right: 2px;
+        padding-left: 2px;
+        padding-top: 0px;
+        padding-bottom: 0px;
+        border-style: solid;
+        border-color: black;
+        background-color: white;
+        font-family: $($FormattingOptions.FontTableDataFamily);
+        font-size: $($FormattingOptions.FontTableDataSize);
+    }
 
-        H2 {
-            font-family: $($FormattingOptions.FontHeadingFamily);
-            font-size: $($FormattingOptions.FontHeadingSize);
-        }
+    H2 {
+        font-family: $($FormattingOptions.FontHeadingFamily);
+        font-size: $($FormattingOptions.FontHeadingSize);
+    }
 
-        P {
-            font-family: $($FormattingOptions.FontFamily);
-            font-size: $($FormattingOptions.FontSize);
-        }
-    </style>
-    </head>
+    P {
+        font-family: $($FormattingOptions.FontFamily);
+        font-size: $($FormattingOptions.FontSize);
+    }
+</style>
+</head>
 "@
     return $Head
 }
