2076da93a51c3cde5b8de05a1da68db3f715c293
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2020-09-09 22:01:46 +0200

    update

---
+++ Public/Converts/ConvertTo-JsonLiteral.ps1
@@ -0,0 +1,114 @@
+﻿function ConvertTo-StringByType {
+    param(
+        [Object] $Value,
+        [string] $DateTimeFormat
+    )
+    if ($null -eq $Value ) {
+        ''
+    } elseif ($Value -is [DateTime]) {
+        $($Value).ToString($DateTimeFormat)
+    } else {
+        try {
+            [System.Text.RegularExpressions.Regex]::Unescape($Value)
+        } catch {
+            $Value.Replace('\', "\\")
+        }
+    }
+}
+
+function ConvertTo-JsonLiteral {
+    [cmdletBinding()]
+    param(
+        [Parameter(ValueFromPipeline, ValueFromPipelineByPropertyName, Position = 0)][Array] $Object,
+        [int] $Depth,
+        [switch] $HashTableAsIs,
+        [switch] $AsArray,
+        [string] $DateTimeFormat = "yyyy-MM-dd HH:mm:ss"
+    )
+    $TextBuilder = [System.Text.StringBuilder]::new()
+    if ($Object.Count -gt 1 -or $AsArray) {
+        $null = $TextBuilder.AppendLine('[')
+    }
+    for ($a = 0; $a -lt $Object.Count; $a++) {
+        if ($Object[$a] -is [System.Collections.IDictionary]) {
+
+            if (-not $HashTableAsIs) {
+                # Push to TEXT the same as [PSCustomObject]
+
+                $null = $TextBuilder.AppendLine("{")
+                for ($i = 0; $i -lt ($Object[$a].Keys).Count; $i++) {
+                    $Property = ([string[]]$Object[$a].Keys)[$i]
+                    <#
+                    if ($Object[$a][$i] -is [DateTime]) {
+                        $Value = $($Object[$a][$i]).ToString($DateTimeFormat)
+                    } else {
+                        $Value = $($Object[$a][$i])
+                    }
+                    #>
+                    $Value = ConvertTo-StringByType -Value $($Object[$a][$i]) -DateTimeFormat $DateTimeFormat
+                    $null = $TextBuilder.Append("`"$Property`":`"$Value`"")
+                    if ($i -ne ($Object[$a].Keys).Count - 1) {
+                        $null = $TextBuilder.AppendLine(',')
+                    }
+                }
+                $null = $TextBuilder.Append("}")
+            } else {
+                # Push to TEXT as real [ordered]
+                $null = $TextBuilder.AppendLine('[')
+                for ($i = 0; $i -lt ($Object[$a].Keys).Count; $i++) {
+                    $null = $TextBuilder.AppendLine("{")
+                    $Property = ([string[]]$Object[$a].Keys)[$i]
+                    #$Value = $Object[$a][$i]
+                    $Value = ConvertTo-StringByType -Value $($Object[$a][$i])
+
+                    $null = $TextBuilder.Append("`"$Property`":`"$Value`"")
+                    $null = $TextBuilder.Append("}")
+                    if ($i -ne ($Object[$a].Keys).Count - 1) {
+                        $null = $TextBuilder.AppendLine(',')
+                    }
+                }
+                $null = $TextBuilder.AppendLine(']')
+            }
+
+        } elseif ($Object[$a].GetType().Name -match 'bool|byte|char|datetime|decimal|double|ExcelHyperLink|float|int|long|sbyte|short|string|timespan|uint|ulong|URI|ushort') {
+            $Value = ConvertTo-StringByType -Value $($Object[$a]) -DateTimeFormat $DateTimeFormat
+            #$null = $TextBuilder.Append("`"$($Object[$a].ToString())`"")
+            $null = $TextBuilder.Append("`"$($Value)`"")
+        } else {
+            $null = $TextBuilder.AppendLine("{")
+            for ($i = 0; $i -lt ($Object[$a].PSObject.Properties.Name).Count; $i++) {
+                $Property = $($Object[$a].PSObject.Properties.Name)[$i]
+                <#
+                if ($($Object[$a].$Property) -is [DateTime]) {
+                    $Value = $($Object[$a].$Property).ToString($DateTimeFormat)
+                } else {
+                    $Value = $($Object[$a].$Property)
+                }
+                #>
+                $Value = ConvertTo-StringByType -Value $($Object[$a].$Property) -DateTimeFormat $DateTimeFormat
+                # Push to Text
+                $null = $TextBuilder.Append("`"$Property`":`"$Value`"")
+                if ($i -ne ($Object[$a].PSObject.Properties.Name).Count - 1) {
+                    $null = $TextBuilder.AppendLine(',')
+                }
+            }
+            $null = $TextBuilder.Append("}")
+        }
+        if ($a -ne $Object.Count - 1) {
+            $null = $TextBuilder.Append(',')
+        }
+    }
+    if ($Object.Count -gt 1 -or $AsArray) {
+        $null = $TextBuilder.AppendLine(']')
+    }
+    $TextBuilder.ToString()
+}
+
+
+Measure-Command {
+    $Test1 = ConvertTo-Json (Get-Process)
+}
+
+Measure-Command {
+    $Test = ConvertTo-JsonLiteral (Get-Process)
+}
---
+++ Tests/ConvertTo-JsonLiteral.Tests.ps1
@@ -0,0 +1,153 @@
+﻿Import-Module .\PSSharedGoods.psd1 -Force
+
+Describe -Name 'Testing ConvertTo-JsonLiteral' {
+    It 'PSCustomObject Conversion' {
+        Enum Fruit{
+            Apple = 29
+            Pear = 30
+            Kiwi = 31
+        }
+        $DateTime = Get-Date
+        $Object = [PSCustomObject] @{
+            Int  = '1'
+            Bool = $false
+            Date = $DateTime
+            Enum = [Fruit]::Kiwi
+        }
+        $Json = ConvertTo-JsonLiteral -Object $Object
+        $FromJson = $Json | ConvertFrom-Json
+
+        $FromJson.Int | Should -Be '1'
+        $FromJson.Bool | Should -Be 'False'
+        $FromJson.Date | Should -Be $DateTime.ToString("yyyy-MM-dd HH:mm:ss")
+        $FromJson.Enum | Should -Be 'Kiwi'
+    }
+    It 'Ordered Dictionary/Hashtable Conversion' {
+        Enum Fruit{
+            Apple = 29
+            Pear = 30
+            Kiwi = 31
+        }
+        $DateTime = Get-Date
+        $Object = [ordered] @{
+            Int  = '1'
+            Bool = $false
+            Date = $DateTime
+            Enum = [Fruit]::Kiwi
+        }
+        $Json = ConvertTo-JsonLiteral -Object $Object
+        $FromJson = $Json | ConvertFrom-Json
+
+        $FromJson.Int | Should -Be '1'
+        $FromJson.Bool | Should -Be 'False'
+        $FromJson.Date | Should -Be $DateTime.ToString("yyyy-MM-dd HH:mm:ss")
+        $FromJson.Enum | Should -Be 'Kiwi'
+    }
+}
+
+Describe -Name 'Testing ConvertTo-JsonLiteral Array' {
+    It 'PSCustomObject Conversion' {
+        Enum Fruit{
+            Apple = 29
+            Pear = 30
+            Kiwi = 31
+        }
+        $DateTime = Get-Date
+        $Object = [PSCustomObject] @{
+            Int  = '1'
+            Bool = $false
+            Date = $DateTime
+            Enum = [Fruit]::Kiwi
+        }
+        $Json = ConvertTo-JsonLiteral -Object $Object, $Object
+        $FromJson = $Json | ConvertFrom-Json
+
+        $FromJson[0].Int | Should -Be '1'
+        $FromJson[0].Bool | Should -Be 'False'
+        $FromJson[0].Date | Should -Be $DateTime.ToString("yyyy-MM-dd HH:mm:ss")
+        $FromJson[0].Enum | Should -Be 'Kiwi'
+        $FromJson[1].Int | Should -Be '1'
+        $FromJson[1].Bool | Should -Be 'False'
+        $FromJson[1].Date | Should -Be $DateTime.ToString("yyyy-MM-dd HH:mm:ss")
+        $FromJson[1].Enum | Should -Be 'Kiwi'
+    }
+    It 'Ordered Dictionary/Hashtable Conversion' {
+        Enum Fruit{
+            Apple = 29
+            Pear = 30
+            Kiwi = 31
+        }
+        $DateTime = Get-Date
+        $Object = [ordered] @{
+            Int  = '1'
+            Bool = $false
+            Date = $DateTime
+            Enum = [Fruit]::Kiwi
+        }
+        $Json = ConvertTo-JsonLiteral -Object $Object, $Object
+        $FromJson = $Json | ConvertFrom-Json
+
+        $FromJson[0].Int | Should -Be '1'
+        $FromJson[0].Bool | Should -Be 'False'
+        $FromJson[0].Date | Should -Be $DateTime.ToString("yyyy-MM-dd HH:mm:ss")
+        $FromJson[0].Enum | Should -Be 'Kiwi'
+        $FromJson[1].Int | Should -Be '1'
+        $FromJson[1].Bool | Should -Be 'False'
+        $FromJson[1].Date | Should -Be $DateTime.ToString("yyyy-MM-dd HH:mm:ss")
+        $FromJson[1].Enum | Should -Be 'Kiwi'
+    }
+}
+
+<#
+return
+
+Enum Fruit{
+    Apple = 29
+    Pear = 30
+    Kiwi = 31
+}
+
+$Object = [PSCustomObject] @{
+    Test         = '1'
+    Test2        = $false
+    Date         = Get-Date
+    Enum         = [Fruit]::Kiwi
+    NestedObject = [PSCustomObject] @{
+        Test  = '1'
+        Test2 = $false
+        Date  = Get-Date
+        Enum  = [Fruit]::Kiwi
+    }
+}
+
+$Object = [PSCustomObject] @{
+    Test  = '1'
+    Test2 = $false
+    Date  = Get-Date
+    Enum  = [Fruit]::Kiwi
+}
+
+$Object1 = [ordered] @{
+    Test  = '1'
+    Test2 = $false
+    Date  = Get-Date
+    Enum  = [Fruit]::Kiwi
+    <#
+    NestedObject = [PSCustomObject] @{
+        Test  = '1'
+        Test2 = $false
+        Date  = Get-Date
+        Enum  = [Fruit]::Kiwi
+    }
+    #>
+}
+
+#$Object, $Object, 1, $false | ConvertTo-Json
+
+#$Json = ConvertTo-JsonLiteral -Object 1, 1, $false, $Object, $Object1
+$Json = ConvertTo-JsonLiteral -Object $Object1 #, $Object #-HashTableAsIs
+$Json
+$Test = $Json | ConvertFrom-Json #| Format-Table
+$Test
+
+#>
