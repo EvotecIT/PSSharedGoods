26616775f4f8d66fa28a21f6abbb7cc3fddb41f0
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2020-09-16 22:49:52 +0200

    Update example

--- Examples/ConvertIdentity4.ps1
+++ Examples/ConvertIdentity4.ps1
@@ -1,4 +1,10 @@
 ﻿Import-Module $PSScriptRoot\..\PSSharedGoods.psd1 -Force
 
-$DN = 'CN=Test Test 2,OU=Users,OU=Production,DC=ad,DC=evotec,DC=pl'
-Convert-Identity -Identity $DN
+@(
+    Convert-Identity -Identity 'CN=Test Test 2,OU=Users,OU=Production,DC=ad,DC=evotec,DC=pl'
+    Convert-Identity -Identity 'NT AUTHORITY\INTERACTIVE'
+    Convert-Identity -Identity 'INTERACTIVE'
+    Convert-Identity -Identity 'EVOTEC\Domain Admins'
+    Convert-Identity -Identity 'EVOTECPL\Domain Admins'
+    Convert-Identity -Identity 'Test\Domain Admins'
+) | Format-Table
