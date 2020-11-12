d51b1ce2813a0415d09638f3142ae4f71a9d8ee5
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2020-11-12 19:20:35 +0100

    Update

--- Public/FilesFolders/Set-FileOwner.ps1
+++ Public/FilesFolders/Set-FileOwner.ps1
@@ -24,12 +24,16 @@
                 if ($JustPath) {
                     $FullPath | ForEach-Object -Process {
                         $File = $_
-                        $ACL = Get-Acl -Path $File
+                        try {
+                            $ACL = Get-Acl -Path $File -ErrorAction Stop
+                        } catch {
+                            Write-Warning "Set-FileOwner - Getting ACL failed with error: $($_.Exception.Message)"
+                        }
                         if ($ACL.Owner -notin $Exlude -and $ACL.Owner -ne $OwnerTranslated) {
                             if ($PSCmdlet.ShouldProcess($File, "Replacing owner $($ACL.Owner) to $OwnerTranslated")) {
                                 try {
                                     $ACL.SetOwner($OwnerTranslated)
-                                    Set-Acl -Path $File -AclObject $ACL
+                                    Set-Acl -Path $File -AclObject $ACL -ErrorAction Stop
                                 } catch {
                                     Write-Warning "Set-FileOwner - Replacing owner $($ACL.Owner) to $OwnerTranslated failed with error: $($_.Exception.Message)"
                                 }
@@ -37,19 +41,26 @@
                         }
                     }
                 } else {
-                    Get-ChildItem -LiteralPath $FullPath -Recurse:$Recursive | ForEach-Object -Process {
+                    Get-ChildItem -LiteralPath $FullPath -Recurse:$Recursive -ErrorAction SilentlyContinue -ErrorVariable err | ForEach-Object -Process {
                         $File = $_
-                        $ACL = Get-Acl -Path $File.FullName
+                        try {
+                            $ACL = Get-Acl -Path $File.FullName -ErrorAction Stop
+                        } catch {
+                            Write-Warning "Set-FileOwner - Getting ACL failed with error: $($_.Exception.Message)"
+                        }
                         if ($ACL.Owner -notin $Exlude -and $ACL.Owner -ne $OwnerTranslated) {
                             if ($PSCmdlet.ShouldProcess($File.FullName, "Replacing owner $($ACL.Owner) to $OwnerTranslated")) {
                                 try {
                                     $ACL.SetOwner($OwnerTranslated)
-                                    Set-Acl -Path $File.FullName -AclObject $ACL
+                                    Set-Acl -Path $File.FullName -AclObject $ACL -ErrorAction Stop
                                 } catch {
                                     Write-Warning "Set-FileOwner - Replacing owner $($ACL.Owner) to $OwnerTranslated failed with error: $($_.Exception.Message)"
                                 }
                             }
                         }
+                    }
+                    foreach ($e in $err) {
+                        Write-Warning "Set-FileOwner - Errors processing $($e.Exception.Message) ($($e.CategoryInfo.Reason))"
                     }
                 }
             }
