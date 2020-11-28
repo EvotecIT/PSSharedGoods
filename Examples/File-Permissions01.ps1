f4564ab1d68e8c553cf605f478150ab2b4e18194
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2020-11-28 10:39:29 +0100

    Update

---
+++ Examples/File-Permissions01.ps1
@@ -0,0 +1,5 @@
+﻿Import-Module $PSScriptRoot\..\PSsharedGoods.psd1 -Force
+
+Get-FilePermissions -Path '\\ad.evotec.xyz\Netlogon\New folder' -Extended | Format-Table
+Get-FilePermissions -Path '\\ad.evotec.pl\Netlogon\New folder (3)' -Extended | Format-Table
+#Get-FilePermissions -Path '\\ad.evotec.xyz\Netlogon\Signatures' | Format-Table
