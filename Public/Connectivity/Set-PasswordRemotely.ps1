9585e526041ca4cf9a7482ceb9a88db9633ae61e
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2020-12-16 10:24:09 +0100

    Improved Set-PasswordRemotly with more secure approach

--- Public/Connectivity/Set-PasswordRemotely.ps1
+++ Public/Connectivity/Set-PasswordRemotely.ps1
@@ -1,20 +1,35 @@
 function Set-PasswordRemotely {
-    [CmdletBinding()]
+    [CmdletBinding(DefaultParameterSetName = 'Secure')]
     param(
-        [Parameter(Mandatory = $true)][string] $UserName,
-        [Parameter(Mandatory = $true)][string] $OldPassword,
-        [Parameter(Mandatory = $true)][string] $NewPassword,
-        [Parameter(Mandatory = $true)][alias('DC', 'Server', 'ComputerName')][string] $DomainController
+        [Parameter(ParameterSetName = 'Secure', Mandatory)][string] $UserName,
+        [Parameter(ParameterSetName = 'Secure', Mandatory)][securestring] $OldPassword,
+        [Parameter(ParameterSetName = 'Secure', Mandatory)][securestring] $NewPassword,
+        [Parameter(ParameterSetName = 'Secure')][alias('DC', 'Server', 'ComputerName')][string] $DomainController
     )
-    $DllImport = @'
+    Begin {
+        $DllImport = @'
 [DllImport("netapi32.dll", CharSet = CharSet.Unicode)]
 public static extern bool NetUserChangePassword(string domain, string username, string oldpassword, string newpassword);
 '@
+        $NetApi32 = Add-Type -MemberDefinition $DllImport -Name 'NetApi32' -Namespace 'Win32' -PassThru
 
-    $NetApi32 = Add-Type -MemberDefinition $DllImport -Name 'NetApi32' -Namespace 'Win32' -PassThru
-    if ($result = $NetApi32::NetUserChangePassword($DomainController, $UserName, $OldPassword, $NewPassword)) {
-        Write-Output -InputObject 'Password change failed. Please try again.'
-    } else {
-        Write-Output -InputObject 'Password change succeeded.'
+        if (-not $DomainController) {
+            #$DomainController = $Env:LOGONSERVER -replace "\\"
+            $Domain = $Env:USERDNSDOMAIN
+            $Context = [System.DirectoryServices.ActiveDirectory.DirectoryContext]::new([System.DirectoryServices.ActiveDirectory.DirectoryContextType]::Domain, $Domain)
+            $DomainController = ([System.DirectoryServices.ActiveDirectory.DomainController]::FindOne($Context)).Name
+
+        }
+    }
+    Process {
+        $OldPasswordPlain = [System.Net.NetworkCredential]::new([string]::Empty, $OldPassword).Password
+        $NewPasswordPlain = [System.Net.NetworkCredential]::new([string]::Empty, $NewPassword).Password
+
+        $result = $NetApi32::NetUserChangePassword($DomainController, $UserName, $OldPasswordPlain, $NewPasswordPlain)
+        if ($result) {
+            Write-Host -Object "Set-PasswordRemotely - Password change for account $UserName failed on $DomainController. Please try again." -ForegroundColor Red
+        } else {
+            Write-Host -Object "Set-PasswordRemotely - Password change for account $UserName succeeded on $DomainController." -ForegroundColor Cyan
+        }
     }
 }
