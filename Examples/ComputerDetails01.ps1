266ca641f50dd348da070803fdb3a83718523aea
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-01-20 19:25:40 +0100

    Update examplee

--- Examples/ComputerDetails01.ps1
+++ Examples/ComputerDetails01.ps1
@@ -1,3 +1,9 @@
 ﻿Import-Module .\PSSharedGoods.psd1 -Force
 
-Get-ComputerOperatingSystem -ComputerName AD1, AD2, AD3, DC1 -All #| ft -a *
+#Get-ComputerOperatingSystem -ComputerName AD1, AD2, AD3, DC1 -All #| ft -a *
+
+Get-ComputerTask | Format-Table
+
+#Get-ScheduledTask -TaskName 'Test' | fl *
+
+#Get-ScheduledTaskInfo -TaskName 'Test' | fl *
