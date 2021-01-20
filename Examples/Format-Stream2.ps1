f21eecd6d12f87e8f1fc9cb805c69386492212f6
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-01-20 19:25:28 +0100

    Update example

--- Examples/Format-Stream2.ps1
+++ Examples/Format-Stream2.ps1
@@ -1,16 +1,16 @@
 #get-process | Select-Object -First 5 | Format-Stream Name, PriorityClass, HandleCount -List -Stream Output # | Add-Content -Path 'C:\Test.txt'
-get-process | Select-Object -First 5 | Format-Stream Name, PriorityClass, HandleCount -List -Stream Debug
-get-process | Select-Object -First 5 | Format-Stream Name, PriorityClass, HandleCount -Stream Output | Add-Content -Path 'C:\Test.txt'
+Get-Process | Select-Object -First 5 | Format-Stream Name, PriorityClass, HandleCount -List -Stream Debug
+Get-Process | Select-Object -First 5 | Format-Stream Name, PriorityClass, HandleCount -Stream Output | Add-Content -Path 'C:\Test.txt'
 
 #get-process | Select-Object -First 5 | Format-TableVerbose Name, PriorityClass, HandleCount #, FileVersion, Handles, Id, Si , StartInfo, MainModule
-get-process | Select-Object -First 5 | Format-Stream Name, PriorityClass, HandleCount , FileVersion, Handles, Id, Si , StartInfo, MainModule -Stream 'Warning'
-get-process | Select-Object -First 5 | Format-Stream Id, Si , StartInfo, MainModule -Stream Information
+Get-Process | Select-Object -First 5 | Format-Stream Name, PriorityClass, HandleCount , FileVersion, Handles, Id, Si , StartInfo, MainModule -Stream 'Warning'
+Get-Process | Select-Object -First 5 | Format-Stream Id, Si , StartInfo, MainModule -Stream Information
 
-get-process | Select-Object -First 5 | Format-Stream *
+Get-Process | Select-Object -First 5 | Format-Stream *
 
-get-process | Select-Object -First 5 | Format-Stream * -HideTableHeaders # Id, Si , StartInfo, MainModule
+Get-Process | Select-Object -First 5 | Format-Stream * -HideTableHeaders # Id, Si , StartInfo, MainModule
 
-get-process | Select-Object -First 5 | Format-Stream * -AlignRight  # Id, Si , StartInfo, MainModule
+Get-Process | Select-Object -First 5 | Format-Stream * -AlignRight  # Id, Si , StartInfo, MainModule
 
-$Process = get-process | Select-Object -First 5
+$Process = Get-Process | Select-Object -First 5
 Format-Stream -InputObject $Process -ExcludeProperty Company, CPU -ColumnHeaderSize 15
