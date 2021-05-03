770934f7514f6c6d9d169b9bd53bb0c444fefec8
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-05-03 23:25:30 +0200

    Improved Remove-Empty with some additional switches

--- Public/Objects/Remove-EmptyValue.ps1
+++ Public/Objects/Remove-EmptyValue.ps1
@@ -5,24 +5,38 @@
         [alias('Splat', 'IDictionary')][Parameter(Mandatory)][System.Collections.IDictionary] $Hashtable,
         [string[]] $ExcludeParameter,
         [switch] $Recursive,
-        [int] $Rerun
+        [int] $Rerun,
+        [switch] $DoNotRemoveNull,
+        [switch] $DoNotRemoveEmpty,
+        [switch] $DoNotRemoveEmptyArray,
+        [switch] $DoNotRemoveEmptyDictionary
     )
     foreach ($Key in [string[]] $Hashtable.Keys) {
         if ($Key -notin $ExcludeParameter) {
             if ($Recursive) {
                 if ($Hashtable[$Key] -is [System.Collections.IDictionary]) {
                     if ($Hashtable[$Key].Count -eq 0) {
-                        $Hashtable.Remove($Key)
+                        if (-not $DoNotRemoveEmptyDictionary) {
+                            $Hashtable.Remove($Key)
+                        }
                     } else {
                         Remove-EmptyValue -Hashtable $Hashtable[$Key] -Recursive:$Recursive
                     }
                 } else {
-                    if ($null -eq $Hashtable[$Key] -or ($Hashtable[$Key] -is [string] -and $Hashtable[$Key] -eq '') -or ($Hashtable[$Key] -is [System.Collections.IList] -and $Hashtable[$Key].Count -eq 0)) {
+                    if (-not $DoNotRemoveNull -and $null -eq $Hashtable[$Key]) {
+                        $Hashtable.Remove($Key)
+                    } elseif (-not $DoNotRemoveEmpty -and $Hashtable[$Key] -is [string] -and $Hashtable[$Key] -eq '') {
+                        $Hashtable.Remove($Key)
+                    } elseif (-not $DoNotRemoveEmptyArray -and $Hashtable[$Key] -is [System.Collections.IList] -and $Hashtable[$Key].Count -eq 0) {
                         $Hashtable.Remove($Key)
                     }
                 }
             } else {
-                if ($null -eq $Hashtable[$Key] -or ($Hashtable[$Key] -is [string] -and $Hashtable[$Key] -eq '') -or ($Hashtable[$Key] -is [System.Collections.IList] -and $Hashtable[$Key].Count -eq 0)) {
+                if (-not $DoNotRemoveNull -and $null -eq $Hashtable[$Key]) {
+                    $Hashtable.Remove($Key)
+                } elseif (-not $DoNotRemoveEmpty -and $Hashtable[$Key] -is [string] -and $Hashtable[$Key] -eq '') {
+                    $Hashtable.Remove($Key)
+                } elseif (-not $DoNotRemoveEmptyArray -and $Hashtable[$Key] -is [System.Collections.IList] -and $Hashtable[$Key].Count -eq 0) {
                     $Hashtable.Remove($Key)
                 }
             }
