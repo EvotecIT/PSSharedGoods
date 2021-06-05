413a5e23de934c04cbce85674c50a3010300f29c
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-06-05 09:59:04 +0200

    Added basic ConvertTo-FlatHashtable

---
+++ Public/Converts/ConvertTo-FlatHashTable.ps1
@@ -0,0 +1,72 @@
+﻿
+function ConvertTo-FlatHashtable {
+    <#
+    .SYNOPSIS
+    Converts nested hashtable into flat hashtable using delimiter
+
+    .DESCRIPTION
+    Converts nested hashtable into flat hashtable using delimiter
+
+    .PARAMETER InputObject
+    Ordered Dictionary or Hashtable
+
+    .EXAMPLE
+    ConvertTo-FlatHashTable -InputObject ([ordered] @{
+        RootEntry       = 'OK1'
+        Parent          = @{
+            Child1 = 'OK2'
+            Child2 = 'Ok3'
+        }
+        ParentDifferent = @{
+            Child7 = 'NotOk'
+            Child8 = @{
+                Child10 = 'OKLetsSee'
+                Child11 = @{
+                    SpecialCase = 'Oooop'
+                }
+            }
+        }
+    }) | Format-Table *
+
+    .NOTES
+    General notes
+    #>
+    [CmdletBinding()]
+    param(
+        [System.Collections.IDictionary] $InputObject
+    )
+    Begin {
+        $Output = [ordered] @{}
+        function Add-HashTableKeys {
+            [CmdletBinding()]
+            param(
+                [System.Collections.IDictionary] $HashTable,
+                [string] $Name
+            )
+            $New = [ordered] @{}
+            foreach ($SubKey in $HashTable.Keys) {
+                $MergedName = -join ($Name, ':', $SubKey)
+                if ($HashTable[$SubKey] -is [System.Collections.IDictionary]) {
+                    $NestedHashtable = Add-Keys -HashTable $HashTable[$SubKey] -Name $MergedName
+                    $New = $New + $NestedHashtable
+                } else {
+                    $New[$MergedName] = $HashTable[$SubKey]
+                }
+            }
+            $New
+        }
+    }
+    Process {
+        foreach ($Key in $InputObject.Keys) {
+            if ($InputObject[$Key] -is [System.Collections.IDictionary]) {
+                $Found = Add-HashTableKeys -HashTable $InputObject[$Key] -Name $Key
+                $Output = $Output + $Found
+            } else {
+                $Output[$Key] = $InputObject[$Key]
+            }
+        }
+    }
+    End {
+        $Output
+    }
+}
