3d3b2acf76d71bd2d7b3b1894b35911009e03d6a
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-10-23 11:13:36 +0200

    Improvements to operating system version naming

--- Public/Converts/ConvertTo-OperatingSystem.ps1
+++ Public/Converts/ConvertTo-OperatingSystem.ps1
@@ -20,6 +20,10 @@
     }
     $Computers | Select-Object DNS*, Name, SamAccountName, Enabled, OperatingSystem*, DistinguishedName | Format-Table
 
+    .EXAMPLE
+    $Registry = Get-PSRegistry -ComputerName 'AD1' -RegistryPath 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion'
+    ConvertTo-OperatingSystem -OperatingSystem $Registry.ProductName -OperatingSystemVersion $Registry.CurrentBuildNumber
+
     .NOTES
     General notes
     #>
@@ -29,12 +33,14 @@
         [string] $OperatingSystemVersion
     )
 
-    if ($OperatingSystem -like '*Windows 10*') {
+    if ($OperatingSystem -like 'Windows 10*' -or $OperatingSystem -like 'Windows 11*') {
         $Systems = @{
             # This is how it's written in AD
+            '10.0 (22000)' = 'Windows 11 21H2'
             '10.0 (19043)' = 'Windows 10 21H1'
             '10.0 (19042)' = 'Windows 10 20H2'
             '10.0 (19041)' = 'Windows 10 2004'
+            '10.0 (18898)' = 'Windows 10 Insider Preview'
             '10.0 (18363)' = "Windows 10 1909"
             '10.0 (18362)' = "Windows 10 1903"
             '10.0 (17763)' = "Windows 10 1809"
@@ -44,12 +50,13 @@
             '10.0 (14393)' = "Windows 10 1607"
             '10.0 (10586)' = "Windows 10 1511"
             '10.0 (10240)' = "Windows 10 1507"
-            '10.0 (18898)' = 'Windows 10 Insider Preview'
 
             # This is how WMI/CIM stores it
+            '10.0.22000'   = 'Windows 11 21H2'
             '10.0.19043'   = 'Windows 10 21H1'
             '10.0.19042'   = 'Windows 10 20H2'
             '10.0.19041'   = 'Windows 10 2004'
+            '10.0.18898'   = 'Windows 10 Insider Preview'
             '10.0.18363'   = "Windows 10 1909"
             '10.0.18362'   = "Windows 10 1903"
             '10.0.17763'   = "Windows 10 1809"
@@ -59,30 +66,70 @@
             '10.0.14393'   = "Windows 10 1607"
             '10.0.10586'   = "Windows 10 1511"
             '10.0.10240'   = "Windows 10 1507"
-            '10.0.18898'   = 'Windows 10 Insider Preview'
+
+            # This is how it's written in registry
+            '22000'        = 'Windows 11 21H2'
+            '19043'        = 'Windows 10 21H1'
+            '19042'        = 'Windows 10 20H2'
+            '19041'        = 'Windows 10 2004'
+            '18898'        = 'Windows 10 Insider Preview'
+            '18363'        = "Windows 10 1909"
+            '18362'        = "Windows 10 1903"
+            '17763'        = "Windows 10 1809"
+            '17134'        = "Windows 10 1803"
+            '16299'        = "Windows 10 1709"
+            '15063'        = "Windows 10 1703"
+            '14393'        = "Windows 10 1607"
+            '10586'        = "Windows 10 1511"
+            '10240'        = "Windows 10 1507"
         }
         $System = $Systems[$OperatingSystemVersion]
         if (-not $System) {
             $System = $OperatingSystem
         }
-    } elseif ($OperatingSystem -like '*Windows Server*') {
+    } elseif ($OperatingSystem -like 'Windows Server*') {
         # May need updates https://docs.microsoft.com/en-us/windows-server/get-started/windows-server-release-info
         # to detect Core
 
         $Systems = @{
-            '5.2 (3790)'   = 'Windows Server 2003'
-            '6.1 (7601)'   = 'Windows Server 2008 R2'
             # This is how it's written in AD
-            '10.0 (18362)' = "Windows Server, version 1903 (Semi-Annual Channel) 1903" # (Datacenter Core, Standard Core)
-            '10.0 (17763)' = "Windows Server 2019 (Long-Term Servicing Channel) 1809" # (Datacenter, Essentials, Standard)
-            '10.0 (17134)' = "Windows Server, version 1803 (Semi-Annual Channel) 1803" # (Datacenter, Standard)
-            '10.0 (14393)' = "Windows Server 2016 (Long-Term Servicing Channel) 1607"
+            '10.0 (20348)' = 'Windows Server 2022'
+            '10.0 (19042)' = 'Windows Server 2019 20H2'
+            '10.0 (19041)' = 'Windows Server 2019 2004'
+            '10.0 (18363)' = 'Windows Server 2019 1909'
+            '10.0 (18362)' = "Windows Server 2019 1903" # (Datacenter Core, Standard Core)
+            '10.0 (17763)' = "Windows Server 2019 1809" # (Datacenter, Essentials, Standard)
+            '10.0 (17134)' = "Windows Server 2016 1803" # (Datacenter, Standard)
+            '10.0 (14393)' = "Windows Server 2016 1607"
+            '6.3 (9600)'   = 'Windows Server 2012 R2'
+            '6.1 (7601)'   = 'Windows Server 2008 R2'
+            '5.2 (3790)'   = 'Windows Server 2003'
 
             # This is how WMI/CIM stores it
-            '10.0.18362'   = "Windows Server, version 1903 (Semi-Annual Channel) 1903" #  (Datacenter Core, Standard Core)
-            '10.0.17763'   = "Windows Server 2019 (Long-Term Servicing Channel) 1809"  # (Datacenter, Essentials, Standard)
-            '10.0.17134'   = "Windows Server, version 1803 (Semi-Annual Channel) 1803" ## (Datacenter, Standard)
-            '10.0.14393'   = "Windows Server 2016 (Long-Term Servicing Channel) 1607"
+            '10.0.20348'   = 'Windows Server 2022'
+            '10.0.19042'   = 'Windows Server 2019 20H2'
+            '10.0.19041'   = 'Windows Server 2019 2004'
+            '10.0.18363'   = 'Windows Server 2019 1909'
+            '10.0.18362'   = "Windows Server 2019 1903" #  (Datacenter Core, Standard Core)
+            '10.0.17763'   = "Windows Server 2019 1809"  # (Datacenter, Essentials, Standard)
+            '10.0.17134'   = "Windows Server 2016 1803" ## (Datacenter, Standard)
+            '10.0.14393'   = "Windows Server 2016 1607"
+            '6.3.9600'     = 'Windows Server 2012 R2'
+            '6.1.7601'     = 'Windows Server 2008 R2' # i think
+            '5.2.3790'     = 'Windows Server 2003' # i think
+
+            # This is how it's written in registry
+            '20348'        = 'Windows Server 2022'
+            '19042'        = 'Windows Server 2019 20H2'
+            '19041'        = 'Windows Server 2019 2004'
+            '18363'        = 'Windows Server 2019 1909'
+            '18362'        = "Windows Server 2019 1903" # (Datacenter Core, Standard Core)
+            '17763'        = "Windows Server 2019 1809" # (Datacenter, Essentials, Standard)
+            '17134'        = "Windows Server 2016 1803" # (Datacenter, Standard)
+            '14393'        = "Windows Server 2016 1607"
+            '9600'         = 'Windows Server 2012 R2'
+            '7601'         = 'Windows Server 2008 R2'
+            '3790'         = 'Windows Server 2003'
         }
         $System = $Systems[$OperatingSystemVersion]
         if (-not $System) {
