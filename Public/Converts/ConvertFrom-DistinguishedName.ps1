4e9d7b1659a7e8124b996c7fd00566d723140e1f
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-12-10 16:58:09 +0100

    Added ToLastName

--- Public/Converts/ConvertFrom-DistinguishedName.ps1
+++ Public/Converts/ConvertFrom-DistinguishedName.ps1
@@ -45,6 +45,24 @@
     Output:
     OU=Production,DC=ad,DC=evotec,DC=xyz
 
+    .EXAMPLE
+    $Con = @(
+        'CN=Windows Authorization Access Group,CN=Builtin,DC=ad,DC=evotec,DC=xyz'
+        'CN=Mmm,DC=elo,CN=nee,DC=RootDNSServers,CN=MicrosoftDNS,CN=System,DC=ad,DC=evotec,DC=xyz'
+        'CN=e6d5fd00-385d-4e65-b02d-9da3493ed850,CN=Operations,CN=DomainUpdates,CN=System,DC=ad,DC=evotec,DC=xyz'
+        'OU=Domain Controllers,DC=ad,DC=evotec,DC=pl'
+        'OU=Microsoft Exchange Security Groups,DC=ad,DC=evotec,DC=xyz'
+    )
+
+    ConvertFrom-DistinguishedName -DistinguishedName $Con -ToLastName
+
+    Output:
+    Windows Authorization Access Group
+    Mmm
+    e6d5fd00-385d-4e65-b02d-9da3493ed850
+    Domain Controllers
+    Microsoft Exchange Security Groups
+
     .NOTES
     General notes
     #>
@@ -55,12 +73,14 @@
         [Parameter(ParameterSetName = 'ToDC')]
         [Parameter(ParameterSetName = 'ToDomainCN')]
         [Parameter(ParameterSetName = 'Default')]
+        [Parameter(ParameterSetName = 'ToLastName')]
         [alias('Identity', 'DN')][Parameter(ValueFromPipeline, ValueFromPipelineByPropertyName, Position = 0)][string[]] $DistinguishedName,
         [Parameter(ParameterSetName = 'ToOrganizationalUnit')][switch] $ToOrganizationalUnit,
         [Parameter(ParameterSetName = 'ToMultipleOrganizationalUnit')][alias('ToMultipleOU')][switch] $ToMultipleOrganizationalUnit,
         [Parameter(ParameterSetName = 'ToMultipleOrganizationalUnit')][switch] $IncludeParent,
         [Parameter(ParameterSetName = 'ToDC')][switch] $ToDC,
-        [Parameter(ParameterSetName = 'ToDomainCN')][switch] $ToDomainCN
+        [Parameter(ParameterSetName = 'ToDomainCN')][switch] $ToDomainCN,
+        [Parameter(ParameterSetName = 'ToLastName')][switch] $ToLastName
     )
     Process {
         foreach ($Distinguished in $DistinguishedName) {
@@ -95,6 +115,29 @@
                     $Value
                 }
                 #return [Regex]::Match($DistinguishedName, 'CN=.*?(DC=.*)').Groups[1].Value
+            } elseif ($ToLastName) {
+                # Would be best if it worked, but there is too many edge cases so hand splits seems to be the best solution
+                # Feel free to change it back to regex if you know how ;)
+                <# https://stackoverflow.com/questions/51761894/regex-extract-ou-from-distinguished-name
+                $Regex = "^(?:(?<cn>CN=(?<name>.*?)),)?(?<parent>(?:(?<path>(?:CN|OU).*?),)?(?<domain>(?:DC=.*)+))$"
+                $Found = $Distinguished -match $Regex
+                if ($Found) {
+                    $Matches.name
+                }
+                #>
+                $NewDN = $Distinguished -split ",DC="
+                if ($NewDN[0].Contains(",OU=")) {
+                    [Array] $ChangedDN = $NewDN[0] -split ",OU="
+                } elseif ($NewDN[0].Contains(",CN=")) {
+                    [Array] $ChangedDN = $NewDN[0] -split ",CN="
+                } else {
+                    [Array] $ChangedDN = $NewDN[0]
+                }
+                if ($ChangedDN[0].StartsWith('CN=')) {
+                    $ChangedDN[0] -replace 'CN=', ''
+                } else {
+                    $ChangedDN[0] -replace 'OU=', ''
+                }
             } else {
                 $Regex = '^CN=(?<cn>.+?)(?<!\\),(?<ou>(?:(?:OU|CN).+?(?<!\\),)+(?<dc>DC.+?))$'
                 #$Output = foreach ($_ in $Distinguished) {
