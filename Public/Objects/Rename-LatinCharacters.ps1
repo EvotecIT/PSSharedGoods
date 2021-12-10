0bad0c6e93744da8dd1c8c052f33db232efef6b4
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-12-10 16:59:39 +0100

    Added docs

--- Public/Objects/Rename-LatinCharacters.ps1
+++ Public/Objects/Rename-LatinCharacters.ps1
@@ -1,7 +1,27 @@
-Function Rename-LatinCharacters {
+﻿Function Rename-LatinCharacters {
+    <#
+    .SYNOPSIS
+    Renames a name to a name without special chars.
+
+    .DESCRIPTION
+    Renames a name to a name without special chars.
+
+    .PARAMETER String
+    Provide a string to rename
+
+    .EXAMPLE
+    Rename-LatinCharacters -String 'Przemysław Kłys'
+
+    .EXAMPLE
+    Rename-LatinCharacters -String 'Przemysław'
+
+    .NOTES
+    General notes
+    #>
     [alias('Remove-StringLatinCharacters')]
-    param (
-        [string]$String
+    [cmdletBinding()]
+    param(
+        [string] $String
     )
     [Text.Encoding]::ASCII.GetString([Text.Encoding]::GetEncoding("Cyrillic").GetBytes($String))
 }
