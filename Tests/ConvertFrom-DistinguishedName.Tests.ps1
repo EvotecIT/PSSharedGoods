eb6b660dbb29bde89ae2c059e5c0849793ddf253
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-12-10 16:59:26 +0100

    Added more test

--- Tests/ConvertFrom-DistinguishedName.Tests.ps1
+++ Tests/ConvertFrom-DistinguishedName.Tests.ps1
@@ -110,4 +110,52 @@
             'OU=Production,DC=ad,DC=evotec,DC=xyz'
         )
     }
+    It 'ToLastName Conversion' {
+        $DistinguishedName = @(
+            'CN=Windows Authorization Access Group,CN=Builtin,DC=ad,DC=evotec,DC=xyz'
+            'CN=Mmm,DC=elo,CN=nee,DC=RootDNSServers,CN=MicrosoftDNS,CN=System,DC=ad,DC=evotec,DC=xyz'
+            'CN=e6d5fd00-385d-4e65-b02d-9da3493ed850,CN=Operations,CN=DomainUpdates,CN=System,DC=ad,DC=evotec,DC=xyz'
+            'OU=Domain Controllers,DC=ad,DC=evotec,DC=pl'
+            'OU=Microsoft Exchange Security Groups,DC=ad,DC=evotec,DC=xyz'
+            'CN=Przemyslaw Klys,OU=Users,OU=Production,DC=ad,DC=evotec,DC=xyz'
+            'CN=ITR03_AD Admins,OU=Security,OU=Groups,OU=Production,DC=ad,DC=evotec,DC=xyz'
+            'CN=SADM Testing 2,OU=Special,OU=Accounts,OU=Production,DC=ad,DC=evotec,DC=xyz'
+        )
+
+        $Output = ConvertFrom-DistinguishedName -DistinguishedName $DistinguishedName -ToLastName
+        $Output | Should -Be @(
+            'Windows Authorization Access Group'
+            'Mmm'
+            'e6d5fd00-385d-4e65-b02d-9da3493ed850'
+            'Domain Controllers'
+            'Microsoft Exchange Security Groups'
+            'Przemyslaw Klys'
+            'ITR03_AD Admins'
+            'SADM Testing 2'
+        )
+    }
+    It 'ToLastName Conversion -Pipeline' {
+        $DistinguishedName = @(
+            'CN=Windows Authorization Access Group,CN=Builtin,DC=ad,DC=evotec,DC=xyz'
+            'CN=Mmm,DC=elo,CN=nee,DC=RootDNSServers,CN=MicrosoftDNS,CN=System,DC=ad,DC=evotec,DC=xyz'
+            'CN=e6d5fd00-385d-4e65-b02d-9da3493ed850,CN=Operations,CN=DomainUpdates,CN=System,DC=ad,DC=evotec,DC=xyz'
+            'OU=Domain Controllers,DC=ad,DC=evotec,DC=pl'
+            'OU=Microsoft Exchange Security Groups,DC=ad,DC=evotec,DC=xyz'
+            'CN=Przemyslaw Klys,OU=Users,OU=Production,DC=ad,DC=evotec,DC=xyz'
+            'CN=ITR03_AD Admins,OU=Security,OU=Groups,OU=Production,DC=ad,DC=evotec,DC=xyz'
+            'CN=SADM Testing 2,OU=Special,OU=Accounts,OU=Production,DC=ad,DC=evotec,DC=xyz'
+        )
+
+        $Output = $DistinguishedName | ConvertFrom-DistinguishedName -ToLastName
+        $Output | Should -Be @(
+            'Windows Authorization Access Group'
+            'Mmm'
+            'e6d5fd00-385d-4e65-b02d-9da3493ed850'
+            'Domain Controllers'
+            'Microsoft Exchange Security Groups'
+            'Przemyslaw Klys'
+            'ITR03_AD Admins'
+            'SADM Testing 2'
+        )
+    }
 }
