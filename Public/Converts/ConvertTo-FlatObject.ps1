2d66ed9ae6ea539ff9e9f641bc9bb80942cb5c07
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2022-02-13 18:31:28 +0100

    Improved flat to object

--- Public/Converts/ConvertTo-FlatObject.ps1
+++ Public/Converts/ConvertTo-FlatObject.ps1
@@ -76,51 +76,61 @@
         [Parameter(DontShow)][String[]]$Path,
         [Parameter(DontShow)][System.Collections.IDictionary] $OutputObject
     )
-    $PipeLine = $Input | ForEach-Object { $_ }
-    If ($PipeLine) {
-        $Objects = $PipeLine
+    Begin {
+        # $PipeLine = $Input | ForEach-Object { $_ }
+        # If ($PipeLine) {
+        #     $Objects = $PipeLine
+        # }
+        $InputObjects = [System.Collections.Generic.List[Object]]::new()
     }
-    If ($PSBoundParameters.ContainsKey("OutputObject")) {
-        $Object = $Objects[0]
-        $Iterate = [ordered] @{}
-        if ($null -eq $Object) {
-            #Write-Verbose -Message "ConvertTo-FlatObject - Object is null"
-        } elseif ($Object.GetType().Name -in 'String', 'DateTime', 'TimeSpan', 'Version', 'Enum') {
-            $Object = $Object.ToString()
-        } elseif ($Depth) {
-            $Depth--
-            If ($Object -is [System.Collections.IDictionary]) {
-                $Iterate = $Object
-            } elseif ($Object -is [Array]) {
-                $i = $Base
-                foreach ($Item in $Object.GetEnumerator()) {
-                    $Iterate["$i"] = $Item
-                    $i += 1
-                }
-            } else {
-                foreach ($Prop in $Object.PSObject.Properties) {
-                    if ($Prop.IsGettable) {
-                        $Iterate["$($Prop.Name)"] = $Object.$($Prop.Name)
+    Process {
+        foreach ($O in $Objects) {
+            $InputObjects.Add($O)
+        }
+    }
+    End {
+        If ($PSBoundParameters.ContainsKey("OutputObject")) {
+            $Object = $InputObjects[0]
+            $Iterate = [ordered] @{}
+            if ($null -eq $Object) {
+                #Write-Verbose -Message "ConvertTo-FlatObject - Object is null"
+            } elseif ($Object.GetType().Name -in 'String', 'DateTime', 'TimeSpan', 'Version', 'Enum') {
+                $Object = $Object.ToString()
+            } elseif ($Depth) {
+                $Depth--
+                If ($Object -is [System.Collections.IDictionary]) {
+                    $Iterate = $Object
+                } elseif ($Object -is [Array]) {
+                    $i = $Base
+                    foreach ($Item in $Object.GetEnumerator()) {
+                        $Iterate["$i"] = $Item
+                        $i += 1
+                    }
+                } else {
+                    foreach ($Prop in $Object.PSObject.Properties) {
+                        if ($Prop.IsGettable) {
+                            $Iterate["$($Prop.Name)"] = $Object.$($Prop.Name)
+                        }
                     }
                 }
             }
-        }
-        If ($Iterate.Keys.Count) {
-            foreach ($Key in $Iterate.Keys) {
-                ConvertTo-FlatObject -Objects @(, $Iterate["$Key"]) -Separator $Separator -Base $Base -Depth $Depth -Path ($Path + $Key) -OutputObject $OutputObject
+            If ($Iterate.Keys.Count) {
+                foreach ($Key in $Iterate.Keys) {
+                    ConvertTo-FlatObject -Objects @(, $Iterate["$Key"]) -Separator $Separator -Base $Base -Depth $Depth -Path ($Path + $Key) -OutputObject $OutputObject
+                }
+            } else {
+                $Property = $Path -Join $Separator
+                #$Property = (($Path | Where-Object { $_ }) -Join $Separator)
+                $OutputObject[$Property] = $Object
             }
-        } else {
-            $Property = $Path -Join $Separator
-            #$Property = (($Path | Where-Object { $_ }) -Join $Separator)
-            $OutputObject[$Property] = $Object
+        } elseif ($InputObjects.Count -gt 0) {
+            $Output = [System.Collections.Generic.List[Object]]::new()
+            foreach ($ItemObject in $InputObjects) {
+                $OutputObject = [ordered]@{}
+                ConvertTo-FlatObject -Objects @(, $ItemObject) -Separator $Separator -Base $Base -Depth $Depth -Path $Path -OutputObject $OutputObject
+                $Output.Add([PSCustomObject] $OutputObject)
+            }
+            $Output
         }
-    } elseif ($null -ne $Objects) {
-        $Output = [System.Collections.Generic.List[Object]]::new()
-        foreach ($ItemObject in $Objects) {
-            $OutputObject = [ordered]@{}
-            ConvertTo-FlatObject -Objects @(, $ItemObject) -Separator $Separator -Base $Base -Depth $Depth -Path $Path -OutputObject $OutputObject
-            $Output.Add([PSCustomObject] $OutputObject)
-        }
-        $Output
     }
 }
