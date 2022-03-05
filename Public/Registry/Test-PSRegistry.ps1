da89420e6d23a4b9d113e127aa5273bd28788b34
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2022-03-05 22:11:00 +0100

    Added new command

---
+++ Public/Registry/Test-PSRegistry.ps1
@@ -0,0 +1,23 @@
+﻿function Test-PSRegistry {
+    [cmdletbinding()]
+    param(
+        [alias('Path')][string[]] $RegistryPath,
+        [string] $ComputerName = $Env:COMPUTERNAME,
+        [string] $Key
+    )
+
+    $Output = Get-PSRegistry -RegistryPath $RegistryPath -ComputerName $ComputerName
+    if ($Output.PSConnection -eq $true -and $Output.PSError -eq $false) {
+        if ($Key) {
+            if ($null -ne $Output.$Key) {
+                return $true
+            } else {
+                return $false
+            }
+        } else {
+            return $true
+        }
+    } else {
+        return $false
+    }
+}
