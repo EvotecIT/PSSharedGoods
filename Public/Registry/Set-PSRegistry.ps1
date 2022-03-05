ba94bf0254ac13773ceac2aa59c210bbe1ae3d40
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2022-03-05 21:40:10 +0100

    Added WhatIf, rewritten using NET

--- Public/Registry/Set-PSRegistry.ps1
+++ Public/Registry/Set-PSRegistry.ps1
@@ -1,84 +1,139 @@
 ﻿function Set-PSRegistry {
-    [cmdletbinding()]
+    [cmdletbinding(SupportsShouldProcess)]
     param(
         [string[]] $ComputerName = $Env:COMPUTERNAME,
         [Parameter(Mandatory)][string] $RegistryPath,
-        [Parameter(Mandatory)][ValidateSet('REG_SZ', 'REG_EXPAND_SZ', 'REG_BINARY', 'REG_DWORD', 'REG_MULTI_SZ', 'REG_QWORD')][string] $Type,
+        [Parameter(Mandatory)][ValidateSet('REG_SZ', 'REG_EXPAND_SZ', 'REG_BINARY', 'REG_DWORD', 'REG_MULTI_SZ', 'REG_QWORD', 'string', 'binary', 'dword', 'qword', 'multistring', 'expandstring')][string] $Type,
         [Parameter(Mandatory)][string] $Key,
         [Parameter(Mandatory)][object] $Value
-        # [switch] $Force
     )
-    #if ($null -eq $Value -and $Force -eq $false) {
-    #    Write-Warning "Set-PSRegistry - Setting registry to $RegistryPath on $($ComputerName -join ', ') was terminated. Value is null."
-    #    return
-    #}
     [Array] $ComputersSplit = Get-ComputerSplit -ComputerName $ComputerName
-    # https://docs.microsoft.com/en-us/previous-versions/windows/desktop/regprov/setstringvalue-method-in-class-stdregprov
-    [uint32] $RootKey = $null
 
-    $RootKeyDictionary = @{
-        HKEY_CLASSES_ROOT   = 2147483648 #(0x80000000)
-        HKCR                = 2147483648 #(0x80000000)
-        HKEY_CURRENT_USER   = 2147483649 #(0x80000001)
-        HKCU                = 2147483649 #(0x80000001)
-        HKEY_LOCAL_MACHINE  = 2147483650 #(0x80000002)
-        HKLM                = 2147483650 #(0x80000002)
-        HKEY_USERS          = 2147483651 #(0x80000003)
-        HKU                 = 2147483651 #(0x80000003)
-        HKEY_CURRENT_CONFIG = 2147483653 #(0x80000005)
-        HKCC                = 2147483653 #(0x80000005)
-        HKEY_DYN_DATA       = 2147483654 #(0x80000006)
-        HKDD                = 2147483654 #(0x80000006)
+    $Dictionary = @{
+        'HKCR:' = 'HKEY_CLASSES_ROOT'
+        'HKCU:' = 'HKEY_CURRENT_USER'
+        'HKLM:' = 'HKEY_LOCAL_MACHINE'
+        'HKU:'  = 'HKEY_USERS'
+        'HKCC:' = 'HKEY_CURRENT_CONFIG'
+        'HKDD:' = 'HKEY_DYN_DATA'
+        'HKPD:' = 'HKEY_PERFORMANCE_DATA'
     }
-    $TypesDictionary = @{
-        'REG_SZ'        = 'SetStringValue' #'REG_SZ'
-        'REG_EXPAND_SZ' = 'SetExpandedStringValue' #'REG_EXPAND_SZ'
-        'REG_BINARY'    = 'SetBinaryValue' # REG_BINARY
-        'REG_DWORD'     = 'SetDWORDValue' #'REG_DWORD'
-        'REG_MULTI_SZ'  = 'SetMultiStringValue' # REG_MULTI_SZ
-        'REG_QWORD'     = 'SetQWORDValue' # 'REG_QWORD'
-        # https://docs.microsoft.com/en-us/previous-versions/windows/desktop/regprov/stdregprov
+
+    # We need to supporrt a lot of options and clean the registry path a bit
+    If ($RegistryPath -like '*:*') {
+        foreach ($DictionaryKey in $Dictionary.Keys) {
+            if ($RegistryPath.StartsWith($DictionaryKey, [System.StringComparison]::CurrentCultureIgnoreCase)) {
+                $RegistryPath = $RegistryPath -replace $DictionaryKey, $Dictionary[$DictionaryKey]
+                break
+            }
+        }
     }
-    $MethodName = $TypesDictionary["$($Type)"]
-    $Arguments = foreach ($_ in $RootKeyDictionary.Keys) {
+    # Remove additional
+    $RegistryPath = $RegistryPath.Replace("\\", "\")
+
+    $HiveDictionary = @{
+        'HKEY_CLASSES_ROOT'      = 'ClassesRoot'
+        'HKCR'                   = 'ClassesRoot'
+        'HKCU'                   = 'CurrentUser'
+        'HKEY_CURRENT_USER'      = 'CurrentUser'
+        'HKLM'                   = 'LocalMachine'
+        'HKEY_LOCAL_MACHINE'     = 'LocalMachine'
+        'HKU'                    = 'Users'
+        'HKEY_USERS'             = 'Users'
+        'HKCC'                   = 'CurrentConfig'
+        'HKEY_CURRENT_CONFIG'    = 'CurrentConfig'
+        'HKDD'                   = 'DynData'
+        'HKEY_DYN_DATA'          = 'DynData'
+        'HKPD'                   = 'PerformanceData'
+        'HKEY_PERFORMANCE_DATA ' = 'PerformanceData '
+    }
+
+    $ReverseTypesDictionary = @{
+        'REG_SZ'        = 'string'
+        'REG_EXPAND_SZ' = 'expandstring'
+        'REG_BINARY'    = 'binary'
+        'REG_DWORD'     = 'dword'
+        'REG_MULTI_SZ'  = 'multistring'
+        'REG_QWORD'     = 'qword'
+        'string'        = 'string'
+        'expandstring'  = 'expandstring'
+        'binary'        = 'binary'
+        'dword'         = 'dword'
+        'multistring'   = 'multistring'
+        'qword'         = 'qword'
+    }
+
+    foreach ($_ in $HiveDictionary.Keys) {
         if ($RegistryPath.StartsWith($_, [System.StringComparison]::CurrentCultureIgnoreCase)) {
-            $RootKey = [uint32] $RootKeyDictionary[$_]
-            $RegistryValue = @{
-                hDefKey     = [uint32] $RootKeyDictionary[$_]
-                sSubKeyName = $RegistryPath.substring($_.Length + 1)
-                sValueName  = $Key
+            $RegistryValue = [ordered] @{
+                HiveKey    = $HiveDictionary[$_]
+                SubKeyName = $RegistryPath.substring($_.Length + 1)
+                ValueKind  = [Microsoft.Win32.RegistryValueKind]::($ReverseTypesDictionary[$Type])
+                Key        = $Key
             }
             if ($Type -in ('REG_SZ', 'REG_EXPAND_SZ', 'REG_MULTI_SZ')) {
-                $RegistryValue['sValue'] = [string] $Value
+                $RegistryValue['Value'] = [string] $Value
             } elseif ($Type -in ('REG_DWORD', 'REG_QWORD')) {
-                $RegistryValue['uValue'] = [uint32] $Value
+                $RegistryValue['Value'] = [uint32] $Value
             } elseif ($Type -in ('REG_BINARY')) {
-                $RegistryValue['uValue'] = [uint8] $Value
+                $RegistryValue['Value'] = [uint8] $Value
             }
-            $RegistryValue
             break
         }
     }
-    foreach ($Computer in $ComputersSplit[0]) {
-        # Local computer
-        try {
-            $ReturnValues = Invoke-CimMethod -Namespace root\cimv2 -ClassName StdRegProv -MethodName $MethodName -Arguments $Arguments -ErrorAction Stop -Verbose:$false
-            if ($ReturnValues.ReturnValue -ne 0) {
-                Write-Warning "Set-PSRegistry - Setting registry to $RegistryPath on $Computer may have failed. Please verify."
+
+    if ($RegistryValue.HiveKey) {
+        foreach ($Computer in $ComputersSplit[0]) {
+            # Local computer
+            try {
+                if ($PSCmdlet.ShouldProcess($Computer, "Setting registry $($RegistryValue.HiveKey)\$($RegistryValue.SubKeyName) on $($RegistryValue.Key) to $($RegistryValue.Value) of $($RegistryValue.ValueKind)")) {
+                    $BaseHive = [Microsoft.Win32.RegistryKey]::OpenBaseKey($RegistryValue.HiveKey, 0 )
+                    $SubKey = $BaseHive.OpenSubKey($RegistryValue.SubKeyName, $true)
+                    if (-not $SubKey) {
+                        $SubKeysSplit = $RegistryValue.SubKeyName.Split('\')
+                        $SubKey = $BaseHive.OpenSubKey($SubKeysSplit[0], $true)
+                        foreach ($S in $SubKeysSplit | Select-Object -Skip 1) {
+                            $SubKey = $SubKey.CreateSubKey($S)
+                        }
+                    }
+                    #$SubKey = $BaseHive.OpenSubKey($RegistryValue.SubKeyName, $true)
+                    $SubKey.SetValue($RegistryValue.Key, $RegistryValue.Value, $RegistryValue.ValueKind)
+                }
+
+                # $ReturnValues = Invoke-CimMethod -Namespace root\cimv2 -ClassName StdRegProv -MethodName $MethodName -Arguments $Arguments -ErrorAction Stop -Verbose:$false
+                # if ($ReturnValues.ReturnValue -ne 0) {
+                #Write-Warning "Set-PSRegistry - Setting registry to $RegistryPath on $Computer may have failed. Please verify."
+                # }
+            } catch {
+                Write-Warning "Set-PSRegistry - Setting registry to $RegistryPath on $Computer have failed. Error: $($_.Exception.Message)"
             }
-        } catch {
-            Write-Warning "Set-PSRegistry - Setting registry to $RegistryPath on $Computer have failed. Error: $($_.Exception.Message)"
         }
-    }
-    foreach ($Computer in $ComputersSplit[1]) {
-        # Remote computer
-        try {
-            $ReturnValues = Invoke-CimMethod -Namespace root\cimv2 -ClassName StdRegProv -MethodName $MethodName -Arguments $Arguments -ComputerName $Computer -ErrorAction Stop -Verbose:$false
-            if ($ReturnValues.ReturnValue -ne 0) {
-                Write-Warning "Set-PSRegistry - Setting registry to $RegistryPath on $Computer may have failed. Please verify."
+        foreach ($Computer in $ComputersSplit[1]) {
+            # Remote computer
+            try {
+                if ($PSCmdlet.ShouldProcess($Computer, "Setting registry $($RegistryValue.HiveKey)\$($RegistryValue.SubKeyName) on $($RegistryValue.Key) to $($RegistryValue.Value) of $($RegistryValue.ValueKind)")) {
+                    $BaseHive = [Microsoft.Win32.RegistryKey]::OpenRemoteBaseKey($RegistryValue.HiveKey, $Computer, 0 )
+                    $SubKey = $BaseHive.OpenSubKey($RegistryValue.SubKeyName, $true)
+                    if (-not $SubKey) {
+                        $SubKeysSplit = $RegistryValue.SubKeyName.Split('\')
+                        $SubKey = $BaseHive.OpenSubKey($SubKeysSplit[0], $true)
+                        foreach ($S in $SubKeysSplit | Select-Object -Skip 1) {
+                            $SubKey = $SubKey.CreateSubKey($S)
+                        }
+                    }
+                    #$SubKey = $BaseHive.OpenSubKey($RegistryValue.SubKeyName, $true)
+                    $SubKey.SetValue($RegistryValue.Key, $RegistryValue.Value, $RegistryValue.ValueKind)
+                }
+                #$ReturnValues = Invoke-CimMethod -Namespace root\cimv2 -ClassName StdRegProv -MethodName $MethodName -Arguments $Arguments -ComputerName $Computer -ErrorAction Stop -Verbose:$false
+                #if ($ReturnValues.ReturnValue -ne 0) {
+                #    Write-Warning "Set-PSRegistry - Setting registry to $RegistryPath on $Computer may have failed. Please verify."
+                #}
+            } catch {
+                Write-Warning "Set-PSRegistry - Setting registry to $RegistryPath on $Computer have failed. Error: $($_.Exception.Message)"
             }
-        } catch {
-            Write-Warning "Set-PSRegistry - Setting registry to $RegistryPath on $Computer have failed. Error: $($_.Exception.Message)"
         }
+    } else {
+        # This shouldn't really happen
+        Write-Warning "Set-PSRegistry - Setting registry to $RegistryPath have failed. Couldn't translate HIVE."
     }
 }
