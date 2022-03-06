c87039d28e539f69f1187d3481016d5bb1321fa3
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2022-03-06 16:10:38 +0100

    Update examples

---
+++ Examples/RegistryRemove.ps1
@@ -0,0 +1,4 @@
+﻿Import-Module $PSScriptRoot\..\PSsharedGoods.psd1 -Force
+
+Remove-PSRegistry -RegistryPath "HKEY_CURRENT_USER\Tests\Ok\MaybeNot" -Key "LimitBlankPass1wordUse" -WhatIf
+Remove-PSRegistry -RegistryPath "HKCU:\Tests\Ok" -WhatIf
---
+++ Examples/RegistrySet.ps1
@@ -0,0 +1,22 @@
+﻿Import-Module $PSScriptRoot\..\PSsharedGoods.psd1 -Force
+
+Set-PSRegistry -RegistryPath "HKCU:\\Tests\CurrentControlSet\Control\Lsa" -Key "LimitBlankPasswordUse" -Value "0" -Type REG_DWORD -WhatIf
+Set-PSRegistry -RegistryPath "HKCU:\\Tests\CurrentControlSet\Control\Lsa" -Key "LimitBlankPasswordUse" -Value "0" -Type REG_DWORD -WhatIf
+Set-PSRegistry -RegistryPath "HKCU:\\Tests" -Key "LimitBlankPass1wordUse" -Value "0" -Type REG_DWORD -WhatIf
+Set-PSRegistry -RegistryPath "HKCU:\\Tests\MoreTests\Tests1" -Key "LimitBlankPass1wordUse" -Value "0" -Type REG_DWORD -WhatIf
+
+Test-PSRegistry -RegistryPath "HKEY_CURRENT_USER\Tests\Ok\MaybeNot\More\AndMore"
+Test-PSRegistry -RegistryPath "HKEY_CURRENT_USER\Tests\Ok\MaybeNot\More\AndMore" -Key "LimitBlankPass1wordUse"
+Test-PSRegistry -RegistryPath "HKEY_CURRENT_USER\Tests\Ok\MaybeNot\More\AndMore" -Key "LimitBlankPasswordUse2"
+
+Get-PSRegistry -RegistryPath 'HKLM\SYSTEM\CurrentControlSet\Services\Netlogon\Parameters' -ComputerName AD1 | Format-Table *
+
+Set-PSRegistry -RegistryPath 'HKLM\SYSTEM\CurrentControlSet\Services\NTDS\Diagnostics' -Type REG_DWORD -Key "16 LDAP Interface Events" -Value 2 -ComputerName AD1 -WhatIf
+Set-PSRegistry -RegistryPath 'HKLM\SYSTEM\CurrentControlSet\Services\NTDS\Diagnostics' -Type REG_SZ -Key "LDAP Interface Events" -Value 'test' -ComputerName AD1 -WhatIf
+Set-PSRegistry -RegistryPath 'HKLM\SYSTEM\CurrentControlSet\Services\NTDS\Diagnostics' -Type REG_EXPAND_SZ -Key "LDAP12 Interface Events" -Value 'test' -ComputerName AD1 -WhatIf
+Set-PSRegistry -RegistryPath 'HKLM\SYSTEM\CurrentControlSet\Services\NTDS\Diagnostics' -Type REG_MULTI_SZ -Key "LDAP12123 Interface Events" -Value @('test', 'ok') -ComputerName AD1 -WhatIf
+
+Set-PSRegistry -RegistryPath "HKEY_CURRENT_USER\Tests\Ok\MaybeNot\More\AndMore" -Key "LimitBlankPasswordUse2" -Value 0 -Type REG_DWORD -WhatIf
+Set-PSRegistry -RegistryPath "HKLM\Software\Ok\MaybeNot\More\AndMore" -Key "LimitBlankPasswordUse2" -Value 0 -Type REG_DWORD -ComputerName 'AD1' -WhatIf
+
+Set-PSRegistry -RegistryPath "HKEY_CURRENT_USER\Tests\CurrentControlSet\Control\Lsa" -Key "LimitBlankPasswordUse" -Value "0" -Type REG_DWORD -WhatIf
