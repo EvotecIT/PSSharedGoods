5c60251ec1e8e3237dacc398ab477558a9a85032
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2022-03-06 15:33:07 +0100

    Added example help

--- Public/Registry/Get-PSRegistry.ps1
+++ Public/Registry/Get-PSRegistry.ps1
@@ -1,4 +1,35 @@
 ﻿function Get-PSRegistry {
+    <#
+    .SYNOPSIS
+    Get registry key values.
+
+    .DESCRIPTION
+    Get registry key values.
+
+    .PARAMETER RegistryPath
+    The registry path to get the values from.
+
+    .PARAMETER ComputerName
+    The computer to get the values from. If not specified, the local computer is used.
+
+    .EXAMPLE
+    Get-PSRegistry -RegistryPath 'HKLM\SYSTEM\CurrentControlSet\Services\Netlogon\Parameters' -ComputerName AD1
+
+    .EXAMPLE
+    Get-PSRegistry -RegistryPath 'HKLM\SYSTEM\CurrentControlSet\Services\Netlogon\Parameters'
+
+    .EXAMPLE
+    Get-PSRegistry -RegistryPath "HKLM\SYSTEM\CurrentControlSet\Services\DFSR\Parameters" -ComputerName AD1,AD2,AD3 | ft -AutoSize
+
+    .EXAMPLE
+    Get-PSRegistry -RegistryPath 'HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\EventLog\Directory Service'
+
+    .EXAMPLE
+    Get-PSRegistry -RegistryPath 'HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\EventLog\Windows PowerShell' | Format-Table -AutoSize
+
+    .NOTES
+    General notes
+    #>
     [cmdletbinding()]
     param(
         [alias('Path')][string[]] $RegistryPath,
