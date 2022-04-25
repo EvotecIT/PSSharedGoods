d7ba7c1a1d45001a16a4c6ff9d80e4f1a19f5b9b
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2022-04-25 13:08:34 +0200

    Return object during WhatIf

--- Private/Set-PrivateRegistry.ps1
+++ Private/Set-PrivateRegistry.ps1
@@ -11,24 +11,24 @@
             return
         }
     }
-    if ($PSCmdlet.ShouldProcess($Computer, "Setting registry $($RegistryValue.HiveKey)\$($RegistryValue.SubKeyName) on $($RegistryValue.Key) to $($RegistryValue.Value) of $($RegistryValue.ValueKind)")) {
-        try {
-            if ($Remote) {
-                $BaseHive = [Microsoft.Win32.RegistryKey]::OpenRemoteBaseKey($RegistryValue.HiveKey, $Computer, 0 )
-            } else {
-                $BaseHive = [Microsoft.Win32.RegistryKey]::OpenBaseKey($RegistryValue.HiveKey, 0 )
-            }
-            $PSConnection = $true
-            $PSError = $null
-        } catch {
-            $PSConnection = $false
-            $PSError = $($_.Exception.Message)
-            if ($PSBoundParameters.ErrorAction -eq 'Stop') {
-                throw
-            } else {
-                Write-Warning "Set-PSRegistry - Setting registry $($RegistryValue.HiveKey)\$($RegistryValue.SubKeyName) on $($RegistryValue.Key) to $($RegistryValue.Value) of $($RegistryValue.ValueKind) on $Computer have failed. Error: $($_.Exception.Message.Replace([System.Environment]::NewLine, " "))"
-            }
+    try {
+        if ($Remote) {
+            $BaseHive = [Microsoft.Win32.RegistryKey]::OpenRemoteBaseKey($RegistryValue.HiveKey, $Computer, 0 )
+        } else {
+            $BaseHive = [Microsoft.Win32.RegistryKey]::OpenBaseKey($RegistryValue.HiveKey, 0 )
         }
+        $PSConnection = $true
+        $PSError = $null
+    } catch {
+        $PSConnection = $false
+        $PSError = $($_.Exception.Message)
+        if ($PSBoundParameters.ErrorAction -eq 'Stop') {
+            throw
+        } else {
+            Write-Warning "Set-PSRegistry - Setting registry $($RegistryValue.HiveKey)\$($RegistryValue.SubKeyName) on $($RegistryValue.Key) to $($RegistryValue.Value) of $($RegistryValue.ValueKind) on $Computer have failed. Error: $($_.Exception.Message.Replace([System.Environment]::NewLine, " "))"
+        }
+    }
+    if ($PSCmdlet.ShouldProcess($Computer, "Setting registry $($RegistryValue.HiveKey)\$($RegistryValue.SubKeyName) on $($RegistryValue.Key) to $($RegistryValue.Value) of $($RegistryValue.ValueKind)")) {
         if ($PSError) {
             if (-not $Suppress) {
                 [PSCustomObject] @{
@@ -94,6 +94,19 @@
                         Type           = $RegistryValue.ValueKind
                     }
                 }
+            }
+        }
+    } else {
+        if (-not $Suppress) {
+            [PSCustomObject] @{
+                PSComputerName = $Computer
+                PSConnection   = $PSConnection
+                PSError        = $true
+                PSErrorMessage = if ($PSError) { $PSError } else { "WhatIf used - skipping registry setting" }
+                Path           = "$($RegistryValue.HiveKey)\$($RegistryValue.SubKeyName)"
+                Key            = $RegistryValue.Key
+                Value          = $RegistryValue.Value
+                Type           = $RegistryValue.ValueKind
             }
         }
     }
