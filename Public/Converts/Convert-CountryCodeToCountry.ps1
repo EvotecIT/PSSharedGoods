48546faf2b94819344552ddaf83ecb13359a08e5
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2022-07-05 12:52:53 +0200

    2 new funcitons

---
+++ Public/Converts/Convert-CountryCodeToCountry.ps1
@@ -0,0 +1,52 @@
+﻿function Convert-CountryCodeToCountry {
+    <#
+    .SYNOPSIS
+    Converts a country code to a country name, or when used with a switch to full culture information
+
+    .DESCRIPTION
+    Converts a country code to a country name, or when used with a switch to full culture information
+
+    .PARAMETER CountryCode
+    Country code
+
+    .PARAMETER All
+    Provide full culture information rather than just the country name
+
+    .EXAMPLE
+    Convert-CountryCodeToCountry -CountryCode 'PL'
+
+    .EXAMPLE
+    Convert-CountryCodeToCountry -CountryCode 'PL' -All
+
+    .EXAMPLE
+    $Test = Convert-CountryCodeToCountry
+    $Test['PL']['Culture'] | fl
+    $Test['PL']['RegionInformation']
+
+    .NOTES
+    General notes
+    #>
+    [cmdletBinding()]
+    param(
+        [string] $CountryCode,
+        [switch] $All
+    )
+    $QuickSearch = [ordered] @{}
+    $AllCultures = [cultureinfo]::GetCultures([System.Globalization.CultureTypes]::SpecificCultures)
+    foreach ($Culture in $AllCultures) {
+        $RegionInformation = [System.Globalization.RegionInfo]::new($Culture.LCID)
+        $QuickSearch[$RegionInformation.Name] = @{
+            'Culture'           = $Culture
+            'RegionInformation' = $RegionInformation
+        }
+    }
+    if ($CountryCode) {
+        if ($All) {
+            $QuickSearch[$CountryCode]
+        } else {
+            $QuickSearch[$CountryCode].RegionInformation
+        }
+    } else {
+        $QuickSearch
+    }
+}
---
+++ Public/Converts/Convert-CountryToCountryCode.ps1
@@ -0,0 +1,55 @@
+﻿function Convert-CountryToCountryCode {
+    <#
+    .SYNOPSIS
+    Converts a country name to a country code, or when used with a switch to full culture information
+
+    .DESCRIPTION
+    Converts a country name to a country code, or when used with a switch to full culture information
+
+    .PARAMETER CountryName
+    Country name in it's english name
+
+    .PARAMETER All
+    Provide full culture information rather than just the country code
+
+    .EXAMPLE
+    Convert-CountryToCountryCode -CountryName 'Poland'
+
+    .EXAMPLE
+    Convert-CountryToCountryCode -CountryName 'Poland' -All
+
+    .EXAMPLE
+    $Test = Convert-CountryToCountryCode
+    $Test['Poland']['Culture']
+    $Test['Poland']['RegionInformation']
+
+    .NOTES
+    General notes
+    #>
+    [cmdletBinding()]
+    param(
+        [string] $CountryName,
+        [switch] $All
+    )
+    $QuickSearch = [ordered] @{}
+    $AllCultures = [cultureinfo]::GetCultures([System.Globalization.CultureTypes]::SpecificCultures)
+    foreach ($Culture in $AllCultures) {
+        $RegionInformation = [System.Globalization.RegionInfo]::new($Culture.LCID)
+        $QuickSearch[$RegionInformation.DisplayName] = @{
+            'Culture'           = $Culture
+            'RegionInformation' = $RegionInformation
+        }
+    }
+    if ($CountryName) {
+        if ($All) {
+            $QuickSearch[$CountryName]
+        } else {
+            $QuickSearch[$CountryName].Culture.TwoLetterIsoLanguageName.ToUpper()
+        }
+    } else {
+        $QuickSearch
+    }
+}
+
+
+
