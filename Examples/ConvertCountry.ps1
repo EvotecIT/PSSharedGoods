a6c8973102606f2eb0d9f119624ce4f0645f5eb0
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2022-09-01 10:16:12 +0200

    Update example

---
+++ Examples/ConvertCountry.ps1
@@ -0,0 +1,6 @@
+﻿Import-Module $PSScriptRoot\..\PSsharedGoods.psd1 -Force
+
+Convert-CountryToCountryCode -CountryName 'Poland'
+Convert-CountryToCountryCode -CountryName 'India'
+Convert-CountryCodeToCountry -CountryCode 'PL'
+Convert-CountryCodeToCountry -CountryCode 'POL'
