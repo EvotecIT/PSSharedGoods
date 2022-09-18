9ac2b62a99fd2ee6c2c8475e4c61dbc2e7915439
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2022-09-18 16:00:47 +0200

    Add convert commands

---
+++ Public/ActiveDirectory/Convert-ADGuidToSchema.ps1
@@ -0,0 +1,70 @@
+﻿function Convert-ADGuidToSchema {
+    <#
+    .SYNOPSIS
+    Converts Guid to schema properties
+
+    .DESCRIPTION
+    Converts Guid to schema properties
+
+    .PARAMETER Guid
+    Guid to Convert to Schema Name
+
+    .PARAMETER Domain
+    Domain to query. By default the current domain is used
+
+   .PARAMETER RootDSE
+    RootDSE to query. By default RootDSE is queried from the domain
+
+    .EXAMPLE
+    $T2 = '570b9266-bbb3-4fad-a712-d2e3fedc34dd'
+    $T = [guid] '570b9266-bbb3-4fad-a712-d2e3fedc34dd'
+
+    Convert-ADGuidToSchema -Guid $T
+    Convert-ADGuidToSchema -Guid $T2
+
+    .NOTES
+    General notes
+    #>
+    [alias('Get-WinADDomainGUIDs', 'Get-WinADForestGUIDs')]
+    [cmdletbinding()]
+    param(
+        [string] $Guid,
+        [string] $Domain = $Env:USERDNSDOMAIN,
+        [Microsoft.ActiveDirectory.Management.ADEntity] $RootDSE #,
+        #[switch] $DisplayNameKey
+    )
+    if ($RootDSE) {
+        $Script:RootDSE = $RootDSE
+    } elseif (-not $Script:RootDSE) {
+        $Script:RootDSE = Get-ADRootDSE -Server $Domain
+    } else {
+        $Script:RootDSE = Get-ADRootDSE -Server $Domain
+    }
+    if (-not $Script:ADSchemaMap) {
+        $Script:ADSchemaMap = @{ }
+        $Script:ADSchemaMap.Add('00000000-0000-0000-0000-000000000000', 'All')
+        $Schema = Get-ADObject -SearchBase $Script:RootDSE.schemaNamingContext -LDAPFilter '(schemaIDGUID=*)' -Properties name, schemaIDGUID
+        foreach ($S in $Schema) {
+            # if ($DisplayNameKey) {
+            #$Script:ADSchemaMap["$($S.name)"] = $(([System.GUID]$S.schemaIDGUID).Guid)
+            # } else {
+            $Script:ADSchemaMap["$(([System.GUID]$S.schemaIDGUID).Guid)"] = $S.name
+            # }
+        }
+
+
+        $Extended = Get-ADObject -SearchBase "CN=Extended-Rights,$($Script:RootDSE.configurationNamingContext)" -LDAPFilter '(objectClass=controlAccessRight)' -Properties name, displayName, rightsGUID
+        foreach ($S in $Extended) {
+            #if ($DisplayNameKey) {
+            #$Script:ADSchemaMap["$($S.name)"] = $(([System.GUID]$S.rightsGUID).Guid)
+            #} else {
+            $Script:ADSchemaMap["$(([System.GUID]$S.rightsGUID).Guid)"] = $S.name
+            #}
+        }
+    }
+    if ($Guid) {
+        $Script:ADSchemaMap[$Guid]
+    } else {
+        $Script:ADSchemaMap
+    }
+}
---
+++ Public/ActiveDirectory/Convert-ADSchemaToGuid.ps1
@@ -0,0 +1,65 @@
+﻿function Convert-ADSchemaToGuid {
+    <#
+    .SYNOPSIS
+    Converts name of schema properties to guids
+
+    .DESCRIPTION
+    Converts name of schema properties to guids
+
+    .PARAMETER SchemaName
+    Schema Name to convert to guid
+
+    .PARAMETER All
+    Get hashtable of all schema properties and their guids
+
+    .PARAMETER Domain
+    Domain to query. By default the current domain is used
+
+    .PARAMETER RootDSE
+    RootDSE to query. By default RootDSE is queried from the domain
+
+    .EXAMPLE
+    Convert-ADSchemaToGuid -SchemaName 'ms-Exch-MSO-Forward-Sync-Cookie'
+
+    .NOTES
+    General notes
+    #>
+    [CmdletBinding()]
+    param(
+        [string] $SchemaName,
+        [string] $Domain = $Env:USERDNSDOMAIN,
+        [Microsoft.ActiveDirectory.Management.ADEntity] $RootDSE
+    )
+    if ($RootDSE) {
+        $Script:RootDSE = $RootDSE
+    } elseif (-not $Script:RootDSE) {
+        $Script:RootDSE = Get-ADRootDSE -Server $Domain
+    } else {
+        $Script:RootDSE = Get-ADRootDSE -Server $Domain
+    }
+    # Create a hashtable to store the GUID value of each schema class and attribute
+    if (-not $Script:ADGuidMap) {
+        $Script:ADGuidMap = [ordered] @{
+            'All' = [System.GUID]'00000000-0000-0000-0000-000000000000'
+        }
+        $StandardRights = Get-ADObject -SearchBase $Script:RootDSE.schemaNamingContext -LDAPFilter "(schemaidguid=*)" -Properties lDAPDisplayName, schemaIDGUID
+        foreach ($Guid in $StandardRights) {
+            $Script:ADGuidMap[$Guid.lDAPDisplayName] = ([System.GUID]$Guid.schemaIDGUID).Guid
+            $Script:ADGuidMap[$Guid.Name] = ([System.GUID]$Guid.schemaIDGUID).Guid
+        }
+
+        #Create a hashtable to store the GUID value of each extended right in the forest
+        $ExtendedRightsGuids = Get-ADObject -SearchBase $Script:RootDSE.ConfigurationNamingContext -LDAPFilter "(&(objectclass=controlAccessRight)(rightsguid=*))" -Properties name, displayName, rightsGuid
+        foreach ($Guid in $ExtendedRightsGuids) {
+            #if ($Guid.Name) {
+            $Script:ADGuidMap[$Guid.Name] = ([System.GUID]$Guid.RightsGuid).Guid
+            #}
+            $Script:ADGuidMap[$Guid.DisplayName] = ([System.GUID]$Guid.RightsGuid).Guid
+        }
+    }
+    if ($SchemaName) {
+        $Script:ADGuidMap[$SchemaName]
+    } else {
+        $Script:ADGuidMap
+    }
+}
