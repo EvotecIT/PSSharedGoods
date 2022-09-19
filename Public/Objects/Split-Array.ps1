2dd8095eb80305d682d39c38bbbf9f4ff43a3cc7
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2022-09-19 19:31:10 +0200

    Fix Array

--- Public/Objects/Split-Array.ps1
+++ Public/Objects/Split-Array.ps1
@@ -1,24 +1,33 @@
 function Split-Array {
-    [CmdletBinding()]
     <#
-        .SYNOPSIS
-        Split an array
-        .NOTES
-        Version : July 2, 2017 - implemented suggestions from ShadowSHarmon for performance
-        .PARAMETER inArray
-        A one dimensional array you want to split
-        .EXAMPLE
-        This splits array into multiple arrays of 3
-        Example below wil return 1,2,3  + 4,5,6 + 7,8,9
+    .SYNOPSIS
+    Split an array into multiple arrays of a specified size or by a specified number of elements
 
-        Split-array -inArray @(1,2,3,4,5,6,7,8,9,10) -parts 3
-        .EXAMPLE
-        This splits array into 3 parts regardless of amount of elements
+    .DESCRIPTION
+    Split an array into multiple arrays of a specified size or by a specified number of elements
 
-        Split-array -inArray @(1,2,3,4,5,6,7,8,9,10) -size 3
+    .PARAMETER Objects
+    Lists of objects you would like to split into multiple arrays based on their size or number of parts to split into.
 
-        # Link: https://gallery.technet.microsoft.com/scriptcenter/Split-an-array-into-parts-4357dcc1
+    .PARAMETER Parts
+    Parameter description
+
+    .PARAMETER Size
+    Parameter description
+
+    .EXAMPLE
+    This splits array into multiple arrays of 3
+    Example below wil return 1,2,3  + 4,5,6 + 7,8,9
+    Split-array -Objects @(1,2,3,4,5,6,7,8,9,10) -Parts 3
+
+    .EXAMPLE
+    This splits array into 3 parts regardless of amount of elements
+    Split-array -Objects @(1,2,3,4,5,6,7,8,9,10) -Size 3
+
+    .NOTES
+
     #>
+    [CmdletBinding()]
     param(
         [alias('InArray', 'List')][Array] $Objects,
         [int]$Parts,
@@ -39,5 +48,5 @@ function Split-Array {
         if ($end -ge $Objects.count) { $end = $Objects.count - 1 }
         $outArray.Add(@($Objects[$start..$end]))
     }
-    return , $outArray
+    , $outArray
 }
