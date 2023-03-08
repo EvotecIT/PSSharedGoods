a1a6b079b698acdec6767504a349ea8ac1e694ee
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-03-08 09:03:51 +0100

    Hopefully fixes https://github.com/EvotecIT/Testimo/issues/144

--- Public/Computers/Get-ComputerSMBSharePermissions.ps1
+++ Public/Computers/Get-ComputerSMBSharePermissions.ps1
@@ -5,32 +5,31 @@
         [Parameter(Mandatory = $true)][alias('Name')][string[]] $ShareName
     )
     [Array] $Computers = Get-ComputerSplit -ComputerName $ComputerName
-    $SMB = @(
-        if ($Computers[0].Count -gt 0) {
+    if ($Computers[0].Count -gt 0) {
+        foreach ($Share in $ShareName) {
             try {
-                $Output = Get-SmbShareAccess -Name $ShareName -ErrorAction Stop
+                $Output = Get-SmbShareAccess -Name $Share -ErrorAction Stop
             } catch {
                 $ErrorMessage = $_.Exception.Message
-                Write-Warning "Get-ComputerSMBSharePermissions - Share $ShareName error: $ErrorMessage"
+                Write-Warning -Message "Get-ComputerSMBSharePermissions - Computer $Env:COMPUTERNAME, Share $Share, Error: $ErrorMessage"
             }
             foreach ($_ in $Output) {
                 Add-Member -InputObject $_ -Name 'PSComputerName' -Value $Env:COMPUTERNAME -MemberType NoteProperty -Force
                 $_
             }
         }
-        if ($Computers[1].Count -gt 0) {
+    }
+    if ($Computers[1].Count -gt 0) {
+        foreach ($Share in $ShareName) {
             try {
-                $Output = Get-SmbShareAccess -CimSession $Computers[1] -Name $ShareName -ErrorAction Stop
+                $Output = Get-SmbShareAccess -CimSession $Computers[1] -Name $Share -ErrorAction Stop
             } catch {
                 $ErrorMessage = $_.Exception.Message
-                Write-Warning "Get-ComputerSMBSharePermissions - Share $ShareName error: $ErrorMessage"
+                Write-Warning -Message "Get-ComputerSMBSharePermissions - Computer $($Computers[1]), Share $Share, Error: $ErrorMessage"
             }
             foreach ($_ in $Output) {
                 $_
             }
         }
-    )
-    $SMB
+    }
 }
-
-#Get-ComputerSMBSharePermissions -ShareName Netlogon,Sysvol -ComputerName AD1,AD2
