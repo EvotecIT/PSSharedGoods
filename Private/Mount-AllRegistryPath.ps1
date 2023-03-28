6dedfb5f2c649989a518abe04d0eacf53199318b
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-03-28 19:58:58 +0200

    Mount offline profiles

---
+++ Private/Mount-AllRegistryPath.ps1
@@ -0,0 +1,12 @@
+﻿function Mount-AllRegistryPath {
+    [CmdletBinding()]
+    param(
+        [string] $MountPoint = "HKEY_USERS\"
+    )
+    $AllProfiles = Get-OfflineRegistryProfilesPath
+    foreach ($Profile in $AllProfiles.Keys) {
+        $WhereMount = "$MountPoint\$Profile".Replace("\\", "\")
+        $AllProfiles[$Profile].Status = Mount-PSRegistryPath -MountPoint $WhereMount -FilePath $AllProfiles[$Profile].FilePath
+    }
+    $AllProfiles
+}
