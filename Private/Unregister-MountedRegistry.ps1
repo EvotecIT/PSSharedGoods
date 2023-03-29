a6d061f4b1a40728e88fcc74f59376de8ec59eff
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-03-29 09:46:30 +0200

    Helper function

---
+++ Private/Unregister-MountedRegistry.ps1
@@ -0,0 +1,16 @@
+﻿function Unregister-MountedRegistry {
+    [CmdletBinding()]
+    param(
+
+    )
+    if ($Script:DefaultRegistryMounted) {
+        $null = Dismount-PSRegistryPath -MountPoint "HKEY_USERS\.DEFAULT_USER"
+        $Script:DefaultRegistryMounted = $null
+    }
+    if ($Script:OfflineRegistryMounted) {
+        foreach ($Key in $Script:OfflineRegistryMounted.Keys) {
+            $null = Dismount-PSRegistryPath -MountPoint "HKEY_USERS\$Key"
+        }
+        $Script:OfflineRegistryMounted = $null
+    }
+}
