fbe60da09e2faabbe7da4bc4fa0693ddd0031598
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-04-03 21:56:52 +0200

    add verbose messages

--- Private/Unregister-MountedRegistry.ps1
+++ Private/Unregister-MountedRegistry.ps1
@@ -3,12 +3,14 @@
     param(
 
     )
-    if ($Script:DefaultRegistryMounted) {
+    if ($null -ne $Script:DefaultRegistryMounted) {
         $null = Dismount-PSRegistryPath -MountPoint "HKEY_USERS\.DEFAULT_USER"
+        Write-Verbose -Message "Unregister-MountedRegistry - Dismounting HKEY_USERS\.DEFAULT_USER"
         $Script:DefaultRegistryMounted = $null
     }
-    if ($Script:OfflineRegistryMounted) {
+    if ($null -ne $Script:OfflineRegistryMounted) {
         foreach ($Key in $Script:OfflineRegistryMounted.Keys) {
+            Write-Verbose -Message "Unregister-MountedRegistry - Dismounting HKEY_USERS\$Key"
             $null = Dismount-PSRegistryPath -MountPoint "HKEY_USERS\$Key"
         }
         $Script:OfflineRegistryMounted = $null
