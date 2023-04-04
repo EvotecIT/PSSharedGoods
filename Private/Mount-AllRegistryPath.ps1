5d40311fbc2826692ca591d44c07e2ab8bdd6ed6
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-04-04 08:06:46 +0200

    Move verbose message

--- Private/Mount-AllRegistryPath.ps1
+++ Private/Mount-AllRegistryPath.ps1
@@ -6,8 +6,8 @@
     $AllProfiles = Get-OfflineRegistryProfilesPath
     foreach ($Profile in $AllProfiles.Keys) {
         $WhereMount = "$MountPoint\$Profile".Replace("\\", "\")
-        $AllProfiles[$Profile].Status = Mount-PSRegistryPath -MountPoint $WhereMount -FilePath $AllProfiles[$Profile].FilePath
         Write-Verbose -Message "Mount-OfflineRegistryPath - Mounting $WhereMount to $($AllProfiles[$Profile].FilePath)"
+        $AllProfiles[$Profile].Status = Mount-PSRegistryPath -MountPoint $WhereMount -FilePath $AllProfiles[$Profile].FilePath
     }
     $AllProfiles
 }
