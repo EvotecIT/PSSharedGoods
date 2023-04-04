ca5aa5a1b8a756cc2edc9bc814a96ad1bb7717de
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-04-04 08:07:09 +0200

    Expand Names in some cases Move verbose

--- Private/Mount-DefaultRegistryPath.ps1
+++ Private/Mount-DefaultRegistryPath.ps1
@@ -3,9 +3,10 @@
     param(
         [string] $MountPoint = "HKEY_USERS\.DEFAULT_USER"
     )
-    $DefaultRegistryPath = Get-PSRegistry -RegistryPath 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\ProfileList' -Key 'Default'
+    $DefaultRegistryPath = Get-PSRegistry -RegistryPath 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\ProfileList' -Key 'Default' -ExpandEnvironmentNames
     if ($PSError -ne $true) {
         $PathToNTUser = [io.path]::Combine($DefaultRegistryPath.PSValue, 'NTUSER.DAT')
+        Write-Verbose -Message "Mount-DefaultRegistryPath - Mounting $MountPoint to $PathToNTUser"
         Mount-PSRegistryPath -MountPoint $MountPoint -FilePath $PathToNTUser
     } else {
         if ($PSBoundParameters.ErrorAction -eq 'Stop') {
