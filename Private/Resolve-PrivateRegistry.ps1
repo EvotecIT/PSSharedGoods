8e37b065499f44223a2191f3bd84f43edeaf5fad
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-04-07 20:24:52 +0200

    improve registry handling with colon

--- Private/Resolve-PrivateRegistry.ps1
+++ Private/Resolve-PrivateRegistry.ps1
@@ -12,14 +12,23 @@
             $R = $R.Replace("Users\.DEFAULT_USER", "HKUD")
             $R.Replace('HKEY_USERS\.DEFAULT_USER', "HKUD")
         } elseif ($R -like '*:*') {
+            $Found = $false
             # This makes sure any short HIVES are converted to expected values
             foreach ($DictionaryKey in $Script:Dictionary.Keys) {
-                if ($R.StartsWith($DictionaryKey, [System.StringComparison]::CurrentCultureIgnoreCase)) {
+                $SplitParts = $R.Split("\")
+                $FirstPart = $SplitParts[0]
+                if ($FirstPart -eq $DictionaryKey) {
                     $R -replace $DictionaryKey, $Script:Dictionary[$DictionaryKey]
+                    $Found = $true
                     break
                 }
             }
+            # Lets try to fix things for user if he uses LONG HIVE names but still with colon
+            if (-not $Found) {
+                $R.Replace(":", "")
+            }
         } else {
+            # This makes sure we do any string without ":" as literal and follow what user wants
             $R
         }
     }
