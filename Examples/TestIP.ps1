551aa2e1ceaed8c2391e4600ced73916c615dac7
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-12-20 18:24:04 +0100

    Add new example

---
+++ Examples/TestIP.ps1
@@ -0,0 +1,11 @@
+﻿Import-Module .\PSSharedGoods.psd1 -Force
+
+$CidrObject = @{
+    Ip            = '10.2.10.0'
+    NetworkLength = 24
+}
+Get-IPAddressRangeInformation -CIDRObject $CidrObject | Format-Table
+
+Get-IPAddressRangeInformation -Network '10.2.10.0/24' | Format-Table
+
+Get-IPAddressRangeInformation -IPAddress '10.2.10.0' -NetworkLength 24 | Format-Table
