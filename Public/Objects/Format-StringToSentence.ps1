05a94a43977a46d6a5d6b93f6d218c74d40d1e65
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-04-26 08:46:11 +0200

    refactor: Add Format-StringToSentence function and remove Format-AddSpaceToSentence function

---
+++ Public/Objects/Format-StringToSentence.ps1
@@ -0,0 +1,110 @@
+function Format-StringToSentence {
+    <#
+    .SYNOPSIS
+    Formats a given string by adding spaces before uppercase letters, digits, and non-word characters.
+
+    .DESCRIPTION
+    The Format-AddSpaceToSentence function takes a string or an array of strings
+    and adds a space before each uppercase letter, digit, and non-word character (excluding dots, spaces, and underscores).
+    It also provides options to convert the string to lowercase, remove certain characters before or after the formatting, and remove double spaces.
+
+    .PARAMETER Text
+    The string or array of strings to be formatted.
+
+    .PARAMETER RemoveCharsBefore
+    An array of characters to be removed from the string before the formatting is applied.
+
+    .PARAMETER RemoveCharsAfter
+    An array of characters to be removed from the string after the formatting is applied.
+
+    .PARAMETER ToLowerCase
+    If this switch is present, the function will convert the string to lowercase.
+
+    .PARAMETER RemoveDoubleSpaces
+    If this switch is present, the function will remove any double spaces from the string.
+
+    .EXAMPLE
+    $test = @(
+        'OnceUponATime',
+        'OnceUponATime1',
+        'Money@Risk',
+        'OnceUponATime123',
+        'AHappyMan2014'
+        'OnceUponATime_123'
+        'Domain test.com'
+    )
+
+    Format-StringToSentence -Text $Test -RemoveCharsAfter '_' -RemoveDoubleSpaces
+
+    This example formats each string in the $test array, removes any underscores after the formatting, and removes any double spaces.
+
+    .EXAMPLE
+    $test = @(
+        'OnceUponATime',
+        'OnceUponATime1',
+        'Money@Risk',
+        'OnceUponATime123',
+        'AHappyMan2014'
+        'OnceUponATime_123'
+        'Domain test.com'
+    )
+
+    $Test | Format-StringToSentence -ToLowerCase -RemoveCharsAfter '_' -RemoveDoubleSpaces
+
+    This example does the same as the previous one, but also converts each string to lowercase.
+
+    .EXAMPLE
+    $test = @(
+        'OnceUponATime',
+        'OnceUponATime1',
+        'Money@Risk',
+        'OnceUponATime123',
+        'AHappyMan2014'
+        'OnceUponATime_123'
+        'Domain test.com'
+    )
+
+    Format-StringToSentence -Text $Test -RemoveCharsAfter '_' -RemoveDoubleSpaces -MakeWordsUpperCase 'test.com', 'money'
+
+    .NOTES
+    The function uses the -creplace operator to add spaces, which is case-insensitive. Therefore, it will add spaces before both uppercase and lowercase letters if they are specified in the RemoveCharsBefore or RemoveCharsAfter parameters.
+    #>
+    [alias('Format-AddSpaceToSentence')]
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory = $false, ValueFromPipeline = $true, Position = 0)][string[]] $Text,
+        [string[]] $RemoveCharsBefore,
+        [string[]] $RemoveCharsAfter,
+        [switch] $ToLowerCase,
+        [switch] $RemoveDoubleSpaces,
+        [string[]] $MakeWordsUpperCase
+    )
+    Process {
+        foreach ($T in $Text) {
+            if ($RemoveCharsBefore) {
+                foreach ($R in $RemoveCharsBefore) {
+                    $T = $T -ireplace [regex]::Escape($R), ""
+                }
+            }
+            $T = $T -creplace '([A-Z]|[^a-zA-Z0-9_.\s]|_|\d+)(?<![a-z])', ' $&'
+            if ($ToLowerCase) {
+                $T = $T.ToLower()
+            }
+            if ($RemoveCharsAfter) {
+                foreach ($R in $RemoveCharsAfter) {
+                    $T = $T -ireplace [regex]::Escape($R), ""
+                }
+            }
+            if ($RemoveDoubleSpaces) {
+                $T = $T -creplace '\s+', ' '
+            }
+            if ($MakeWordsUpperCase) {
+                foreach ($W in $MakeWordsUpperCase) {
+                    $T = $T -ireplace [regex]::Escape($W), $W.ToUpper()
+
+                }
+            }
+            $T.Trim()
+        }
+    }
+}
