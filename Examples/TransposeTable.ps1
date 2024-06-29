a13189c26248cd3030fae2d5a2b982fdc8b7f463
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-06-29 18:05:05 +0200

    Update example

---
+++ Examples/TransposeTable.ps1
@@ -0,0 +1,86 @@
+﻿Clear-Host
+
+Import-Module .\PSSharedGoods.psd1 -Force
+
+# $T = [PSCustomObject] @{
+#     Test   = 1
+#     Test2  = 7
+#     Ole    = 'bole'
+#     Trolle = 'A'
+#     Alle   = 'sd'
+# }
+
+# $T1 = [PSCustomObject] @{
+#     Test   = 1
+#     Test2  = 7
+#     Ole    = 'bole'
+#     Trolle = 'A'
+#     Alle   = 'sd'
+# }
+
+# $T2 = [ordered] @{
+#     Test   = 1
+#     Test2  = 7
+#     Ole    = 'bole'
+#     Trolle = 'A'
+#     Alle   = 'sd'
+# }
+# $T3 = @{
+#     Test   = 1
+#     Test2  = 7
+#     Ole    = 'bole'
+#     Trolle = 'A'
+#     Alle   = 'sd'
+# }
+
+# Format-TransposeTable -Object @($T) -Sort ASC -Legacy | Format-Table
+# Format-TransposeTable -Object @($T) -Sort DESC -Legacy | Format-Table
+# Format-TransposeTable -Object @($T) -Legacy | Format-Table
+
+# Format-TransposeTable -Object @($T2) -Sort ASC -Legacy | Format-Table
+# Format-TransposeTable -Object @($T2) -Sort DESC -Legacy | Format-Table
+# Format-TransposeTable -Object @($T2) -Legacy | Format-Table
+
+$T = [PSCustomObject] @{
+    Onet   = 'Test'
+    Name   = "Server 1"
+    Test   = 1
+    Test2  = 7
+    Ole    = 'bole'
+    Trolle = 'A'
+    Alle   = 'sd'
+}
+$T1 = [PSCustomObject] @{
+    Name   = "Server 2"
+    Test   = 2
+    Test2  = 3
+    Ole    = '1bole'
+    Trolle = 'A'
+    Alle   = 'sd'
+}
+
+Format-TransposeTable -Object @($T, $T1) -Property "Name" | Format-Table
+
+$T2 = [ordered] @{
+    Onet   = 'Test'
+    Name   = "Server 1"
+    Test   = 1
+    Test2  = 7
+    Ole    = 'bole'
+    Trolle = 'A'
+    Alle   = 'sd'
+}
+$T3 = [ordered] @{
+    Name   = "Server 2"
+    Test   = 2
+    Test2  = 3
+    Ole    = '1bole'
+    Trolle = 'A'
+    Alle   = 'sd'
+}
+
+$Test = Format-TransposeTable -Object @($T2, $T3)
+$Test | Format-Table
+
+
+# Format-TransposeTable -Object @($T) | Format-Table
