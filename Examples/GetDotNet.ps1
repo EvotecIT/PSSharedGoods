90f982805a510426d07bc8c86b0ba6b1a90c2a67
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-06-29 19:29:31 +0200

    Example for dotnet

---
+++ Examples/GetDotNet.ps1
@@ -0,0 +1,8 @@
+﻿
+$PSVersionTable
+$PSVersionTable.DotNetVersion = [System.Runtime.InteropServices.RuntimeInformation]::FrameworkDescription
+$PSVersionTable.OSArchitecture = [System.Runtime.InteropServices.RuntimeInformation]::OSArchitecture
+
+[version]([System.Runtime.InteropServices.RuntimeInformation]::FrameworkDescription -split '\s' | Select-Object -Last 1)
+
+[version][regex]::Match([System.Runtime.InteropServices.RuntimeInformation]::FrameworkDescription, '(\d\.?){3,}$').Value
