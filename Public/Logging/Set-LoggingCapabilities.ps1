5c53c896b6743b51e7397e6cfb9c24b3c0a415e9
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-12-22 09:37:24 +0100

    Add Set-LoggingCapabilities and Set-ReportingCapabilities functions for enhanced logging and reporting management

---
+++ Public/Logging/Set-LoggingCapabilities.ps1
@@ -0,0 +1,97 @@
+﻿function Set-LoggingCapabilities {
+    <#
+    .SYNOPSIS
+    Sets up logging capabilities by managing log files.
+
+    .DESCRIPTION
+    This function sets up logging capabilities by creating the necessary directories and managing the number of log files based on the specified maximum.
+
+    .PARAMETER LogPath
+    The path where the log files will be stored.
+
+    .PARAMETER ScriptPath
+    The path of the script that generates the logs.
+
+    .PARAMETER LogMaximum
+    The maximum number of log files to keep. Older files will be deleted if this limit is exceeded.
+
+    .PARAMETER ShowTime
+    Switch to include timestamps in the log entries.
+
+    .PARAMETER TimeFormat
+    The format of the timestamps in the log entries.
+
+    .EXAMPLE
+    Set-LoggingCapabilities -LogPath "C:\Logs\log.log" -ScriptPath "C:\Scripts\script.ps1" -LogMaximum 10 -ShowTime -TimeFormat "yyyy-MM-dd HH:mm:ss"
+
+    .NOTES
+    This function is used in:
+    - CleanupMonster
+    - PasswordSolution
+    - SharePointEssentials
+
+    And many other scripts.
+    #>
+    [CmdletBinding()]
+    param(
+        [Alias('Path', 'Log', 'Folder', 'LiteralPath', 'FilePath')][string] $LogPath,
+        [string] $ScriptPath,
+        [Alias('Maximum')][int] $LogMaximum,
+        [switch] $ShowTime,
+        [string] $TimeFormat
+    )
+
+    $Script:PSDefaultParameterValues = @{
+        "Write-Color:LogFile"    = $LogPath
+        "Write-Color:ShowTime"   = if ($PSBoundParameters.ContainsKey('ShowTime')) { $ShowTime.IsPresent } else { $null }
+        "Write-Color:TimeFormat" = $TimeFormat
+    }
+    if ($LogPath) {
+        try {
+            $FolderPath = [io.path]::GetDirectoryName($LogPath)
+            if (-not (Test-Path -LiteralPath $FolderPath)) {
+                $null = New-Item -Path $FolderPath -ItemType Directory -Force -WhatIf:$false
+            }
+            if ($LogMaximum -gt 0) {
+                if ($ScriptPath) {
+                    $ScriptPathFolder = [io.path]::GetDirectoryName($ScriptPath)
+                    if ($ScriptPathFolder -eq $FolderPath) {
+                        Write-Color -Text '[i] ', "LogMaximum is set to ", $LogMaximum, " but log files are in the same folder as the script. Cleanup disabled." -Color Yellow, White, DarkCyan, White
+                        return
+                    }
+                    # Get the extension of the log file
+                    $LogPathExtension = [io.path]::GetExtension($LogPath)
+                    # Get the log files, sort them by creation time, and skip the first $LogMaximum
+                    if ($LogPathExtension) {
+                        # If the log file has an extension, filter the files by that extension to prevent deleting other files
+                        $CurrentLogs = Get-ChildItem -LiteralPath $FolderPath -Filter "*$LogPathExtension" -ErrorAction Stop | Sort-Object -Property CreationTime -Descending | Select-Object -Skip $LogMaximum
+                    } else {
+                        $CurrentLogs = $null
+                        Write-Color -Text '[i] ', "Log file has no extension (?!). Cleanup disabled." -Color Yellow, White, DarkCyan, White
+                    }
+                    if ($CurrentLogs) {
+                        Write-Color -Text '[i] ', "Logs directory has more than ", $LogMaximum, " log files. Cleanup required..." -Color Yellow, DarkCyan, Red, DarkCyan
+                        foreach ($Log in $CurrentLogs) {
+                            try {
+                                Remove-Item -LiteralPath $Log.FullName -Confirm:$false -WhatIf:$false
+                                Write-Color -Text '[+] ', "Deleted ", "$($Log.FullName)" -Color Yellow, White, Green
+                            } catch {
+                                Write-Color -Text '[-] ', "Couldn't delete log file $($Log.FullName). Error: ', "$($_.Exception.Message) -Color Yellow, White, Red
+                            }
+                        }
+                    }
+                } else {
+                    Write-Color -Text '[i] ', "LogMaximum is set to ", $LogMaximum, " but no script path detected. Most likely running interactively. Cleanup disabled." -Color Yellow, White, DarkCyan, White
+                }
+            } else {
+                Write-Color -Text '[i] ', "LogMaximum is set to 0 (Unlimited). No log files will be deleted." -Color Yellow, DarkCyan
+            }
+        } catch {
+            Write-Color -Text "[e] ", "Couldn't create the log directory. Error: $($_.Exception.Message)" -Color Yellow, Red
+            $Script:PSDefaultParameterValues["Write-Color:LogFile"] = $null
+        }
+    } else {
+        $Script:PSDefaultParameterValues["Write-Color:LogFile"] = $null
+    }
+    Remove-EmptyValue -Hashtable $Script:PSDefaultParameterValues
+}
---
+++ Public/Logging/Set-ReportingCapabilities.ps1
@@ -0,0 +1,77 @@
+﻿function Set-ReportingCapabilities {
+    <#
+    .SYNOPSIS
+    Sets up reporting capabilities by managing report files.
+
+    .DESCRIPTION
+    This function sets up reporting capabilities by creating the necessary directories and managing the number of report files based on the specified maximum.
+
+    .PARAMETER ReportPath
+    The path where the report files will be stored.
+
+    .PARAMETER ScriptPath
+    The path of the script that generates the reports.
+
+    .PARAMETER ReportMaximum
+    The maximum number of report files to keep. Older files will be deleted if this limit is exceeded.
+
+    .EXAMPLE
+    Set-ReportingCapabilities -ReportPath "C:\Reports\report.log" -ScriptPath "C:\Scripts\script.ps1" -ReportMaximum 10
+
+    .NOTES
+    This function is used in:
+    - CleanupMonster
+    - PasswordSolution
+    - SharePointEssentials
+
+    And many other scripts.
+    #>
+    [CmdletBinding()]
+    param(
+        [alias('Path', 'LiteralPath', 'FilePath')][string] $ReportPath,
+        [string] $ScriptPath,
+        [int] $ReportMaximum
+    )
+    if ($ReportPath) {
+        try {
+            $FolderPath = [io.path]::GetDirectoryName($ReportPath)
+            if (-not (Test-Path -LiteralPath $FolderPath -ErrorAction Stop)) {
+                $null = New-Item -Path $FolderPath -ItemType Directory -Force -WhatIf:$false -ErrorAction Stop
+            }
+            if ($ReportMaximum -gt 0) {
+                if ($ScriptPath) {
+                    $ScriptPathFolder = [io.path]::GetDirectoryName($ScriptPath)
+                    if ($ScriptPathFolder -eq $FolderPath) {
+                        Write-Color -Text '[i] ', "ReportMaximum is set to ", $ReportMaximum, " but report files are in the same folder as the script. Cleanup disabled." -Color Yellow, White, DarkCyan, White
+                        return
+                    }
+                }
+                # Get the extension of the report file
+                $ReportPathExtension = [io.path]::GetExtension($ReportPath)
+                # Get the report files, sort them by creation time, and skip the first $ReportMaximum
+                if ($ReportPathExtension) {
+                    # If the report file has an extension, filter the files by that extension to prevent deleting other files
+                    $CurrentReports = Get-ChildItem -LiteralPath $FolderPath -Filter "*$ReportPathExtension" -ErrorAction Stop | Sort-Object -Property CreationTime -Descending | Select-Object -Skip $ReportMaximum
+                } else {
+                    $CurrentReports = $null
+                    Write-Color -Text '[i] ', "Report file has no extension (?!). Cleanup disabled." -Color Yellow, White, DarkCyan, White
+                }
+                if ($CurrentReports) {
+                    Write-Color -Text '[i] ', "Reporting directory has more than ", $ReportMaximum, " report files. Cleanup required..." -Color Yellow, DarkCyan, Red, DarkCyan
+                    foreach ($Report in $CurrentReports) {
+                        try {
+                            Remove-Item -LiteralPath $Report.FullName -Confirm:$false -WhatIf:$false
+                            Write-Color -Text '[+] ', "Deleted ", "$($Report.FullName)" -Color Yellow, White, Green
+                        } catch {
+                            Write-Color -Text '[-] ', "Couldn't delete report file $($Report.FullName). Error: ', "$($_.Exception.Message) -Color Yellow, White, Red
+                        }
+                    }
+                }
+            } else {
+                Write-Color -Text '[i] ', "ReportMaximum is set to 0 (Unlimited). No report files will be deleted." -Color Yellow, DarkCyan
+            }
+        } catch {
+            Write-Color -Text "[e] ", "Couldn't create the reporting directory. Error: $($_.Exception.Message)" -Color Yellow, Red
+        }
+    }
+}
